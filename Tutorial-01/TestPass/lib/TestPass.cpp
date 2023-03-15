671d664e81d7a590b3ca678b79d8bb4ed9e2bcb3
Alex <ferrarini.alex24@gmail.com>
2023-03-15 21:04:12 +0100

    Committed tasks for Lab1

--- Tutorial-01/TestPass/lib/TestPass.cpp
+++ Tutorial-01/TestPass/lib/TestPass.cpp
@@ -13,8 +13,29 @@ public:
            << "\n";
 
       // TODO: Completare il metodo come indicato per il LAB1.
-    
-    }
+      for(auto iterM = M.begin(); iterM != M.end(); ++iterM){
+        Function &F = *iterM;
+        outs()<<F.getName()<<"\n";
+        int contBB=0,contCall=0;
+        //Conteggio basic blocks
+        for(auto iterF = F.begin(); iterF != F.end(); ++iterF){
+          BasicBlock &BB = *iterF;
+          contBB++;
+          for(auto iterBB = BB.begin(); iterBB != BB.end(); ++iterBB){
+            Instruction &Inst = *iterBB;
+            if(CallInst *call_inst=dyn_cast<CallInst>(&Inst))
+              contCall++;
+          }
+        }
+        outs()<<"\tNumero Basic Blocks: "<<contBB;
+        if(F.isVarArg()){
+          outs()<<"\n\tNumero Argomenti: "<<F.arg_size()<<"+*";
+        }else{
+          outs()<<"\n\tNumero Argomenti: "<<F.arg_size();
+        }
+        outs()<<"\n\tNumero Istruzioni: "<<F.getInstructionCount();
+        outs()<<"\n\tFunzioni Chiamate: "<<contCall<<"\n\n";
+      }
 
     return PreservedAnalyses::all();
   }
@@ -38,6 +59,6 @@ extern "C" PassPluginLibraryInfo llvmGetPassPluginInfo() {
                   }
                   return false;
                 });
-          } 
+          }
   };
 }
