5ee0487566e334cab911a9741543e4ce2df9787b
Riccardo Cracco <281925@studenti.unimore.it>
2023-03-17 10:49:30 +0100

    Risoluzione esercizio 3.

--- Tutorial-01/TestPass/test/Loop.c
+++ Tutorial-01/TestPass/test/Loop.c
@@ -17,9 +17,10 @@ int g;
 // Numero di chiamate: 0
 // Numero di BB: 1
 // Numero di Istruzioni: 4
-int g_incr(int c) {
-  g += c;
-  return g;
+int g_incr(int c)
+{
+    g += c;
+    return g;
 }
 
 // Nome della funzione: loop
@@ -27,12 +28,14 @@ int g_incr(int c) {
 // Numero di chiamate: 0
 // Numero di BB: 3
 // Numero di Istruzioni: 10
-int loop(int a, int b, int c) {
-  int i, ret = 0;
+int loop(int a, int b, int c)
+{
+    int i, ret = 0;
 
-  for (i = a; i < b; i++) {
-    g_incr(c);
-  }
+    for (i = a; i < b; i++)
+    {
+        g_incr(c);
+    }
 
-  return ret + g;
+    return ret + g;
 }
