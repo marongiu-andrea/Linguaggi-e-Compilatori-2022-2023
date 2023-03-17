5e10edab50b6a21177a0bd1c73ea87737da21c49
simone <simonevaiasinni@gmail.com>
2023-03-17 15:48:24 +0100

    Tutorial01 Completato!

--- Tutorial-01/TestPass/lib/TestPass.cpp
+++ Tutorial-01/TestPass/lib/TestPass.cpp
@@ -11,29 +11,61 @@ namespace {
     -passes=test-pass test/LoopO0.ll -o test/LoopTestPass.ll
   */
 
-  class TestPass final : public PassInfoMixin<TestPass> {
+  class TestPass final : public PassInfoMixin<TestPass> 
+  {
   public:
     PreservedAnalyses run([[maybe_unused]] Module &M, ModuleAnalysisManager &) 
     {
-      outs() << "Passo di test per il corso di Linguaggi e Compilatori\n";
-
-      for(auto it = M.begin(); it != M.end(); ++it)
+      for(auto itMod = M.begin(); itMod != M.end(); ++itMod)
       {
-        Function& func = *it;
+        Function& func = *itMod;
         FunctionType& funcType = *func.getFunctionType();
-        
-        // if(func.isDeclaration())
-        //   outs() << "func " << func << " IS declaration" << "\n";
-        // else
-        //   outs() << "func " << func << " IS NOT declaration" << "\n";
+
+        // Function name & params
+        // ---------------------- 
         const char* funcName = func.getName().data(); 
         const int funcNumParams = funcType.getNumParams();
 
-        outs() << funcName << " " << funcNumParams << "\n";
+        outs() << "FuncName=" << funcName << ", NumParams=" << funcNumParams << "\n";
+        // if(funcType.isVarArg())
+        //   outs() << "+*";
+        // outs() << "\n";
 
+        // Basic blocks
+        // ---------------------- 
+        uint32_t nBlocks = 0;
+        uint32_t nInstructions = 0;
+        uint32_t nFuncCalls = 0;
+        for(auto itBlock = func.begin(); itBlock != func.end(); ++itBlock, ++nBlocks)
+        {
+          BasicBlock& block = *itBlock;
+          
+          // Block instruction
+          // ---------------------- 
+          for(auto itInstr = block.begin(); itInstr != block.end(); ++itInstr, ++nInstructions)
+          {
+            Instruction& instr = *itInstr;
+
+            outs() << "instruction=[" << instr << "  ]";
+
+            CallInst* callInstr = dyn_cast<CallInst>(&instr);
+            if(callInstr)
+            {
+              Function& called = *callInstr->getCalledFunction();
+              outs() << " > Chiamata a funzione: " << called.getName();
+              ++nFuncCalls;
+            }
+            outs() << "\n";
+          }
+
+        } 
+
+        outs() << "Numero di chiamate a funzione nello stesso modulo=" << nFuncCalls << "\n";
+        outs() << "Numero di Basic Blocks=" << nBlocks << "\n";
+        outs() << "Numero di Istruzioni=" << nInstructions << "\n";
+        outs() << "--------------------------------------------\n";
       }
 
-      // TODO: Completare il metodo come indicato per il LAB1.
       return PreservedAnalyses::all();
     }
   }; // class TestPass
