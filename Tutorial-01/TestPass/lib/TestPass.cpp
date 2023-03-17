b5501adf4aba23e3c43b74f9a993ee57696f65a0
Nickbarbieri1 <barbieri.nic01@gmail.com>
2023-03-17 11:38:20 +0100

    Soluzione alla prima esercitazione di Barbieri Nicolo e Montanari Luca

--- Tutorial-01/TestPass/lib/TestPass.cpp
+++ Tutorial-01/TestPass/lib/TestPass.cpp
@@ -12,9 +12,54 @@ public:
     outs() << "Passo di test per il corso di Linguaggi e Compilatori"
            << "\n";
 
+    int call_func = 0;
+
+    for(auto iter=M.begin(); iter!=M.end(); ++iter){
+      Function &F = *iter;
+      //outs() << F.getName() << "\n" << "Numero di argomenti: " << F.arg_size() << "\n";
+      
+      if (F.isVarArg()){
+      	outs() << F.getName() << "\n" << "Numero di argomenti: " << F.arg_size() << "+*\n";
+      }
+      else {
+      	outs() << F.getName() << "\n" << "Numero di argomenti: " << F.arg_size() << "\n";
+      }
+
+
+      int n_basicblock = 0;
+      int n_istruzioni = 0;
+
+      for(BasicBlock &BB : F){
+        n_basicblock += 1;
+        n_istruzioni += BB.size();
+
+        for(Instruction &inst : BB){
+          if(isa<CallInst>(&inst))
+            call_func += 1;
+
+        }
+
+      }  
+
+      //  OPPURE
+      
+      /*
+      if(F.getEntryCount(true).hasValue()) {
+      	outs() << "Numero di Basic Block: " << F.size() << "\nNumero di istruzioni totali: " << F.getInstructionCount() << "\nNumero di chiamate della funzione: " << (F.getEntryCount(true).getValue()).getCount() <<  "\n\n";
+      }
+      else {
+      	outs() << "Numero di Basic Block: " << F.size() << "\nNumero di istruzioni totali: " << F.getInstructionCount() << "\n\n";
+      }
+      */
+      
+      outs() << "Numero di chiamate a funzione: " << call_func << "\n";
+      outs() << "numero di Basic Block: " << n_basicblock <<"\n" << "Numero di istruzioni: "<< n_istruzioni <<"\n\n";
+
+
+    }
       // TODO: Completare il metodo come indicato per il LAB1.
     
-    }
+    //}
 
     return PreservedAnalyses::all();
   }
