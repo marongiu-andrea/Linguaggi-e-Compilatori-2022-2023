f7772cf281570808fae4ba97304dcd2b22f49349
adamtao14 <taoufiq787@gmail.com>
2023-03-18 20:30:28 +0100

    Creazione shift al posto di mul con potenza di 2

--- Tutorial-02/LocalOpts/lib/Transform.cpp
+++ Tutorial-02/LocalOpts/lib/Transform.cpp
@@ -1,11 +1,15 @@
 #include "LocalOpts.h"
 #include "llvm/IR/InstrTypes.h"
 #include "llvm/ADT/APInt.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Constants.h"
 #include <string.h>
 #include <cmath>
 using namespace llvm;
 
 bool runOnBasicBlock(BasicBlock &B) {
+  Function *F = B.getParent();
+  LLVMContext &context = F->getContext();
     
     /*
     // Preleviamo le prime due istruzioni del BB
@@ -59,46 +63,84 @@ bool runOnBasicBlock(BasicBlock &B) {
     Inst1st.replaceAllUsesWith(NewInst);
     */
   int index = 1;
+  Instruction *old_instr = nullptr;
   for(auto Iter = B.begin(); Iter != B.end(); ++Iter){
     Instruction &Inst = *Iter;
 
     if(!strcmp(Inst.getOpcodeName(),"mul")){
-      outs() << "Istruzione numero: " << index++ << "\n";
-      outs() << Inst << "\n";
-      outs() << "Operazione: " << Inst.getOpcodeName() << "\n";
-      //outs() << "MOLTIPLICAZIONE" << "\n";
-      for (auto *Iter_op = Inst.op_begin(); Iter_op != Inst.op_end(); ++Iter_op) {
-        Value *Operand = *Iter_op;
+      
 
-        if (Argument *Arg = dyn_cast<Argument>(Operand)) {
-          outs() << "\t" << *Arg << ": SONO L'ARGOMENTO N. " << Arg->getArgNo() <<" DELLA FUNZIONE" << Arg->getParent()->getName() << "\n";
-        }
-      if (ConstantInt *C = dyn_cast<ConstantInt>(Operand)) {
+      if (ConstantInt *C = dyn_cast<ConstantInt>(Inst.getOperand(1))) {
+        outs() << "Istruzione numero: " << index++ << " è una moltiplicazione \n";
+        outs() << "\tIstruzione: " << Inst << "\n";
         outs() << "\t" << *C << ": SONO UNA COSTANTE INTERA DI VALORE " << C->getValue() << "\n";
 
-        /*
-        Questo serve perchè LLVM restituisce un intero forzato quando si usa getValue()
-        per aggirare la cosa si usa una libreria che estrae l'intero e poi fa il cast a float
-        questo serve perchè la funzione log2 prende un float come parametro.
-        */
-        uint64_t value = C->getValue().getZExtValue();  // extract the integer value from the APInt object
-        float value_in_float = static_cast<float>(value);   
-        float log_value = log2(value_in_float);
+        APInt intVal = C->getValue();
+        float floatVal = static_cast<float>(intVal.getLimitedValue());
+        float log_value = log2(floatVal);
+        
         //se il log_value è pari alla sua floor vuol dire che value_In_float è effettivamente una potenza di 2
         //se non lo fosse  log_value sarebbe un valore con la virgola e quindi non valido
         if(log_value == floor(log_value)){
-          outs() << "Possiamo fare un miglioramento " << "\n";
+          int log_value_int = static_cast<int>(log_value);
+          
+          Value *operand_instr = Inst.getOperand(0);
+
+          ConstantInt* log2ConstInt = ConstantInt::get(context, APInt(32, 1));
+          Instruction *NewInst = BinaryOperator::CreateShl(operand_instr, log2ConstInt, "NewInst");
+          outs() << "\tNew instruction" << *NewInst << "\n";
+          NewInst->insertAfter(old_instr);
+            // Si possono aggiornare le singole references separatamente?
+            // Controlla la documentazione e prova a rispondere.
+          Inst.replaceAllUsesWith(NewInst);
         }
       }
+      /*
+      for (auto *Iter_op = Inst.op_begin(); Iter_op != Inst.op_end(); ++Iter_op) {
+        Value *Operand = *Iter_op; 
+        
+        if (ConstantInt *C = dyn_cast<ConstantInt>(Operand)) {
+          outs() << "\t" << *C << ": SONO UNA COSTANTE INTERA DI VALORE " << C->getValue() << "\n";
 
-    }
+          /*
+          Questo serve perchè LLVM restituisce un intero forzato quando si usa getValue()
+          per aggirare la cosa si usa una libreria che estrae l'intero e poi fa il cast a float
+          questo serve perchè la funzione log2 prende un float come parametro.
+          
+          uint64_t value = C->getValue().getZExtValue();  // extract the integer value from the APInt object
+          float value_in_float = static_cast<float>(value);   
+          float log_value = log2(value_in_float);
+          //se il log_value è pari alla sua floor vuol dire che value_In_float è effettivamente una potenza di 2
+          //se non lo fosse  log_value sarebbe un valore con la virgola e quindi non valido
+          if(log_value == floor(log_value)){
+            outs() << "\tPossiamo fare un miglioramento " << "\n";
+            //outs() << "\t Primo operando: " << Inst.getOperand(0) << "\n";
+            //int value_of_log_in_int = APInt IntValue(32, log_value);
+            /*
+            LLVMContext context;
+            Value *log_value_in_constant_int = ConstantInt::get(Type::getInt32Ty(context),log_value);
+            Instruction *NewInst = BinaryOperator::CreateShl(
+            Inst.getOperand(0), log_value_in_constant_int);
+
+            NewInst->insertAfter(old_instr);
+            // Si possono aggiornare le singole references separatamente?
+            // Controlla la documentazione e prova a rispondere.
+            Inst.replaceAllUsesWith(NewInst);
+            
+            
+          
+          }
+        }
+
+      }
+      */
     /*
     for (auto *Iter = Inst1st.op_begin(); Iter != Inst1st.op_end(); ++Iter) {
       Value *Operand = *Iter;
     */
 
-  }
-    
+    }
+    old_instr = &(*Iter);
     
 
 
