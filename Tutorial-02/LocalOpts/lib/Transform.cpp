c72347b8c810e11453d8fdfd44542fc1c8849f8b
umberto <umberto@Compilatori.myguest.virtualbox.org>
2023-03-21 13:00:20 +0100

    Inserimento istruzione shift dopo le mul incriminate

--- Tutorial-02/LocalOpts/lib/Transform.cpp
+++ Tutorial-02/LocalOpts/lib/Transform.cpp
@@ -2,8 +2,18 @@
 #include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Instructions.h"
+#include "cmath"
 
 using namespace llvm;
+
+bool isPoweOfTwo(int n){
+  if(n>0)
+    return (n & (n-1)) == 0;
+  else
+    return false;
+}
 
 bool runOnBasicBlock(BasicBlock &B) {
     /*
@@ -58,25 +68,54 @@ bool runOnBasicBlock(BasicBlock &B) {
     
     */
     for (Instruction &iter : B){
-
       if(iter.getOpcode() == Instruction::Mul){
-        for (auto *argIter = iter.op_begin(); argIter != iter.op_end(); ++argIter) {
-      Value *Operand = *argIter;
+          int operandPow2=-1, operandNotPow=-1;
+          int value0,value1;
+          double valueF;
+          bool trasformato = false;
+          //Estraggo gli operandi in forma numerica
+          if(ConstantInt *C = dyn_cast<ConstantInt>(iter.getOperand(0))) {
+            value0 = C->getSExtValue();
+            trasformato = true;  
+          }
+          if(ConstantInt *C = dyn_cast<ConstantInt>(iter.getOperand(1))) {
+            value1 = C->getSExtValue();
+            trasformato=true;  
+          }
+          if(!trasformato)
+            continue;
+          //controllo se uno dei due è potenza di 2
+          if(isPoweOfTwo(value0)){
+              operandPow2=0;
+              operandNotPow=1;
+              printf("Il primo operando numerico è potenza di 2: %i\n",value0);
+              valueF=value0;
+            }else if (isPoweOfTwo(value1)){
+              operandPow2=1;
+              operandNotPow=0;
+              printf("Il secondo operando numerico è potenza di 2: %i\n",value1);
+              valueF=value1;
+            }else{
+              //nessuno dei due operandi è potenza di 2
+              continue;
+            }
+            //creo l'operando per la nuova operazione di shift
+            printf("Sono qui\n");
+            ConstantInt *Con = dyn_cast<ConstantInt>(iter.getOperand(operandPow2));
+            printf("Sono qui\n");
+            Value *newOP = ConstantInt::get(Con->getType(),
+              static_cast<int>(log2(valueF)));
+            printf("Sono qui\n");
+            Instruction *NewInst = BinaryOperator::Create(Instruction::Shl, 
+              iter.getOperand(operandNotPow), 
+              newOP);
+              printf("Sono qui\n");
 
-      /*if (Argument *Arg = dyn_cast<Argument>(Operand)) {
-        outs() << "\t" << *Arg << ": SONO L'ARGOMENTO N. " << Arg->getArgNo() 
-	       <<" DELLA FUNZIONE" << Arg->getParent()->getName()
-               << "\n";
-      }*/
-      if (ConstantInt *C = dyn_cast<ConstantInt>(Operand)) {
-        int value = C->getSExtValue();
-        printf("Operando numerico %i\n",value);
-        //TODO
-        //verificare se value è una potenza di due
-        //se è potenza di due, creare una nuova istruzione che lo usi come operando assieme all'altro
-        //sostituire quell'istruzione a quella precedente
-      }
-    }
+            //sostituisco la vecchio mul con la nuova shift
+            NewInst->insertAfter(&iter);
+            //iter.replaceAllUsesWith(NewInst);
+            //iter.eraseFromParent();
+
       }
     };
 
