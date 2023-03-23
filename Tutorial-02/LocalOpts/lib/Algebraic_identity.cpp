bef29cd424ac95bab9fd6803e275816f6d4cb00f
Davide Buchignani <davi28564@gmail.com>
2023-03-23 12:32:19 +0100

    Riconosce identita', deve sostituire

--- Tutorial-02/LocalOpts/lib/Algebraic_identity.cpp
+++ Tutorial-02/LocalOpts/lib/Algebraic_identity.cpp
@@ -4,10 +4,26 @@
 using namespace llvm;
 
 bool runOnBasicBlockAIP(BasicBlock &B) {
-  for(auto &Iter : B){
-    outs() << "Istruzione: " << Iter << "\n";
+  for(auto &Inst : B){
+
+    for(auto *I = Inst.op_begin(); I != Inst.op_end(); ++I){
+      int a;
+      if (ConstantInt *C = dyn_cast<ConstantInt>(I))
+        a = C->getSExtValue();
+
+      if(a == 0 && (Inst.getOpcode() == Instruction::Add || Inst.getOpcode() == Instruction::Sub)){
+        outs()<< "Inutile in add/sub\n";
+
+      }
+      else if(a == 1 && (Inst.getOpcode() == Instruction::Mul || Inst.getOpcode() == Instruction::SDiv)){
+        outs()<< "Inutile in mul/div\n";
+      }
+
+    }
+
+    /*outs() << "Istruzione: " << Inst << "\n";
     outs() <<"\tOperandi: ";
-    for (auto *I = Iter.op_begin(); I != Iter.op_end(); ++I){
+    for (auto *I = Inst.op_begin(); I != Inst.op_end(); ++I){
       if (Argument *Arg = dyn_cast<Argument>(I)) {
         outs() << "\t" << *Arg << ": Argument";
       }
@@ -15,7 +31,7 @@ bool runOnBasicBlockAIP(BasicBlock &B) {
         outs() << "\t" << *C << ": Constant";
       }
     }
-    outs()<<"\n";
+    outs()<<"\n";*/
   }
 };
 
@@ -31,7 +47,7 @@ bool runOnFunctionAIP(Function &F) {
   return Transformed;
 };
 
-PreservedAnalyses  AlgebraicIdentityPass::run([[maybe_unused]] Module &M, ModuleAnalysisManager &) {
+PreservedAnalyses  AlgebraicIdentityPass::run([[maybe_unused]] Module &M, ModuleAnalysisManager   &) {
     for (auto &Iter : M) {
       if (runOnFunctionAIP(Iter)) {
         return PreservedAnalyses::none();
