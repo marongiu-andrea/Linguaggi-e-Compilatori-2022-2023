e80edf36de955bf739852d1996fe004d58f9b944
Nickbarbieri1 <barbieri.nic01@gmail.com>
2023-03-23 13:12:58 +0100

    Implementazione secondo punto della consegna in tutte le sue parti

--- Tutorial-02/LocalOpts/lib/Transform_Strength.cpp
+++ Tutorial-02/LocalOpts/lib/Transform_Strength.cpp
@@ -7,6 +7,26 @@ bool isPowerOfTwoStrength(ConstantInt *C) {
 		return C->getValue().isPowerOf2();
 	}
 
+int checkConstantIntPower2(ConstantInt* C1, ConstantInt* C2) {
+    if (C1 && C1->getValue().isPowerOf2()) { //la costante numerica e' il primo operando
+        return 0;
+    }
+    else if (C2 && C2->getValue().isPowerOf2()){ //la costante numerica e' il secondo operando
+        return 1;
+    }
+    else {
+        return -1;
+    }
+}
+
+int checkConstantValue(ConstantInt* C1, ConstantInt* C2) {
+    if (C1 && !C2) 
+        return 0;
+    if (C2 && !C1)
+        return 1;
+    return -1;
+}
+
 bool runOnBasicBlockStrength(BasicBlock &B) {
     
     // Preleviamo le prime due istruzioni del BB
@@ -57,29 +77,82 @@ bool runOnBasicBlockStrength(BasicBlock &B) {
     // Si possono aggiornare le singole references separatamente?
     // Controlla la documentazione e prova a rispondere.
     Inst1st.replaceAllUsesWith(NewInst);
+    Inst1st.eraseFromParent();
+
+
     
+    //MARK: implementazione punto 2 della consegna
+
 	ConstantInt* CI1;
 	ConstantInt* CI2;
 	Instruction* instrToRemove;
-	bool flag;
+	bool flag=false;
     for (auto &instr : B) {
     	if (flag==true){
     		instrToRemove->eraseFromParent();
     		flag=false;
     	}
-		if(auto *BO = dyn_cast<BinaryOperator>(&instr)){
-			if(BO->getOpcode() == Instruction::SDiv){ //se si tratta di una operazione che posso sostituire con un RSHIFT
-				outs() << "Sono arrivato fino a qui!\n";
-				CI2 = dyn_cast<ConstantInt>(BO->getOperand(1));
-				if(CI2 && isPowerOfTwoStrength(CI2)) {
-					flag=true;
-					instrToRemove=&instr;
-					Value* ShiftAmount = ConstantInt::get(Type::getInt32Ty(B.getContext()), CI2->getValue().logBase2());
-					BinaryOperator* NewInst = BinaryOperator::CreateAShr(BO->getOperand(0),ShiftAmount,"",BO);
-					BO->replaceAllUsesWith(NewInst);
-				}
-			}
-		}
+          if(auto *BO = dyn_cast<BinaryOperator>(&instr)){
+            if(BO->getOpcode() == Instruction::SDiv){ //se si tratta di una operazione che posso sostituire con un RSHIFT
+              CI2 = dyn_cast<ConstantInt>(BO->getOperand(1));
+              if(CI2 && isPowerOfTwoStrength(CI2)) {
+                flag=true;
+                instrToRemove=&instr;
+                Value* ShiftAmount = ConstantInt::get(Type::getInt32Ty(B.getContext()), CI2->getValue().logBase2());
+                BinaryOperator* NewInst = BinaryOperator::CreateAShr(BO->getOperand(0),ShiftAmount,"",BO);
+                BO->replaceAllUsesWith(NewInst);
+              }
+            }
+            else if (BO->getOpcode() == Instruction::Mul) {
+                int constantIndex=-1;
+                int pos_shift;
+                CI1 = dyn_cast<ConstantInt>(BO->getOperand(0));
+                CI2 = dyn_cast<ConstantInt>(BO->getOperand(1));
+                constantIndex = checkConstantIntPower2(CI1,CI2);
+                if (constantIndex != -1) { //sono sicuro di aver trovato una istruzione di moltiplicazione che puo' essere sostituita usando una shift a sx
+                    flag=true;
+                    if (constantIndex==0) {
+                        pos_shift = CI1->getValue().logBase2();
+                    }
+                    else {
+                        pos_shift = CI2->getValue().logBase2();
+                    }
+                    Value* ShiftAmountValue = ConstantInt::get(Type::getInt32Ty(B.getContext()), pos_shift);
+                    Instruction* NewInst = BinaryOperator::CreateShl(BO->getOperand(1-constantIndex),ShiftAmountValue,"",BO);
+                    BO->replaceAllUsesWith(NewInst);
+                    instrToRemove=&instr;
+                }
+                else { //devo valutare se sono dentro ad un caso di quelli in cui sto moltiplicando per il valore precedente a quello di una potenza di 2
+                    constantIndex=checkConstantValue(CI1,CI2);
+                    if (constantIndex != -1) {
+                        APInt value=APInt(32,1,true);
+                        int valueInt;
+                        int operandInt;
+                        if (constantIndex == 0) {
+                            pos_shift = CI1->getValue().nearestLogBase2();
+                            //value = CI1->getValue();
+                            operandInt=CI1->getValue().getSExtValue();
+                        }
+                        else {
+                            pos_shift = CI2->getValue().nearestLogBase2();
+                            //value = CI2->getValue();
+                            operandInt=CI2->getValue().getSExtValue();
+                        }
+                        value=value.shl(pos_shift); //value adesso contiene il valore della potenza di 2 piu' vicina alla costante numerica della moltiplicazione
+                        valueInt = value.getSExtValue();
+                        //outs() << valueInt << " " << operandInt << "\n";
+                        if ((valueInt-operandInt) == 1) {
+                            //outs() << "Sono arrivato fino a qui\n";
+                            Instruction* NewSub = BinaryOperator::CreateSub(ConstantInt::get(Type::getInt32Ty(B.getContext()), valueInt),BO->getOperand(1-constantIndex),"",BO);
+                            BO->replaceAllUsesWith(NewSub);
+                            flag=true;
+                            instrToRemove=&instr;
+                            //TODO: manca da implementare la logica di generazione dell'istruzione di shift, la logica dell'istruzione di sottrazione e poi bisogna sostituire tutte gli user della moltiplicazione CON L'OPERAZIONE DI SOTTRAZIONE
+                        }
+                    }
+                }
+            }
+		  }
     }
 
     return true;
