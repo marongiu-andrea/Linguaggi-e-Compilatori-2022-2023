e8b31ad6b11d9a0d7d53e3434a89631f79d88737
adamtao14 <taoufiq787@gmail.com>
2023-03-25 19:04:05 +0100

    Strenght reduction mul

--- Tutorial-02/LocalOpts/lib/StrengthReduction.cpp
+++ Tutorial-02/LocalOpts/lib/StrengthReduction.cpp
@@ -9,11 +9,82 @@
 
 using namespace llvm;
 
+std::list<Instruction *> instructionsToBeRemoved;
+
+void check_and_create(ConstantInt *operand, Value *operand_to_shift,std::string position, LLVMContext &context, Instruction &inst)
+{
+    
+    Instruction *newShiftInst = nullptr;
+    Instruction *newSubInst = nullptr;
+    APInt value = operand->getValue();
+
+    if (value.isPowerOf2())
+    {
+
+        
+        IntegerType *opType = operand->getType();
+
+        ConstantInt *numShift = ConstantInt::get(opType, value.exactLogBase2());
+        if (value.isNonNegative())
+        {
+            //questo si fa nel caso abbiamo una potenza esatta di 2 e quindi non serve la sub
+            newShiftInst = BinaryOperator::Create(Instruction::Shl, operand_to_shift, numShift);
+            outs() << "\t Nuova istruzione di shift a sinistra di passi: " << numShift->getValue() << " sono a " << position << "\n";
+
+        }
+    }
+    else
+    {
+        //nel caso non sia una potenza esatta di 2, bisogna trovare quella più vicina che sia maggiore
+        int value_int = value.getLimitedValue();
+        double log_value = log2(static_cast<double>(value_int));
+        double shift_len = ceil(log_value);
+        //prendo il valore intero dall'oggetto APInt e faccio la log2,dopo calcolo il ceil del valore ottenuto
+        //questo per avere sempre la prima potenza di 2 maggiore del valore
+        //esempio 15 avrà 4 e 17 avrà 5
+
+        outs() << "\tThe number of shifts of value " << value_int << " is " << shift_len << "\n";
+        //int power_of_log = pow(2,static_cast<int>(shift_len));
+        int shift_len_int = static_cast<int>(shift_len);
+       
+
+        //costanti intere per la shift e la sub
+        ConstantInt *api_shift = ConstantInt::get(context, APInt(32, shift_len_int));
+        ConstantInt *api_sub = ConstantInt::get(context, APInt(32, value_int));
+
+        newShiftInst = BinaryOperator::Create(Instruction::Shl, operand_to_shift, api_shift);
+        newSubInst = BinaryOperator::Create(Instruction::Sub, newShiftInst, api_sub);
+    }
+
+    //la shift c'è sempre siccome siamo in una mul
+    newShiftInst->insertAfter(&inst);
+
+    //la sub bisogna vedere se è stata usata quindi non è null
+    if(newSubInst)
+    {
+        newSubInst->insertAfter(newShiftInst);
+    }
+
+    //nel caso non ci sia una sub rimpiazzo gli usi con la nuova istruzione di shift altrimenti con quella di sub
+    if(newShiftInst &&  !newSubInst)
+    {
+        inst.replaceAllUsesWith(newShiftInst);
+    }
+    else
+    {
+        inst.replaceAllUsesWith(newSubInst);
+    }
+
+    //aggiungo l'istruzione corrente a quelle da elminare
+    instructionsToBeRemoved.push_back(&inst);
+    
+}
+
 bool runOnBasicBlockStrengthReduction(BasicBlock &BB)
 {
-    // Function *F = B.getParent();
-    // LLVMContext &context = F->getContext();
-    std::list<Instruction *> instructionsToBeRemove;
+    Function *F = BB.getParent();
+    LLVMContext &context = F->getContext();
+    
 
     for (auto &inst : BB)
     {
@@ -29,43 +100,29 @@ bool runOnBasicBlockStrengthReduction(BasicBlock &BB)
             ConstantInt *costantLeft = dyn_cast<ConstantInt>(opLeft);
             ConstantInt *costantRight = dyn_cast<ConstantInt>(opRight);
 
-            Instruction *newInst = nullptr;
-
             switch (opcode)
             {
             case Instruction::Mul:
                 outs() << "\tMoltiplicazione: \n";
                 if (costantRight)
+                {   
+                    check_and_create(costantRight,opLeft,"destra",context,inst);
+                } 
+                else if (costantLeft)
                 {
-                    if (costantRight->getValue().isPowerOf2())
-                    {
-                        APInt value = costantRight->getValue();
-                        IntegerType *opType = costantRight->getType();
-
-                        ConstantInt *numShift = ConstantInt::get(opType, value.exactLogBase2());
-                        if (value.isNonNegative())
-                        {
-                            newInst = BinaryOperator::Create(Instruction::Shl, opLeft, numShift);
-                            outs() << "\t Nuova istruzione" << *newInst << "\n";
-                        }
-                    }
+                    check_and_create(costantLeft,opRight,"sinistra",context,inst);
                 }
                 break;
 
             case Instruction::SDiv:
-            case Instruction::UDiv:
                 outs() << "\tDivisione: \n";
+
                 break;
             }
-            if (newInst)
-            {
-                newInst->insertBefore(&inst);
-                inst.replaceAllUsesWith(newInst);
-                instructionsToBeRemove.push_back(&inst);
-            }
+            
         }
     }
-    for (auto i : instructionsToBeRemove)
+    for (auto i : instructionsToBeRemoved)
         i->eraseFromParent();
 
     return true;
--- Tutorial-02/LocalOpts/test/test.optimized.ll
+++ Tutorial-02/LocalOpts/test/test.optimized.ll
@@ -4,13 +4,14 @@ source_filename = "test/test.ll"
 define dso_local void @foo(i32 noundef %0) {
   %2 = add nsw i32 %0, 0
   %3 = shl i32 %2, 4
-  %4 = mul nsw i32 %3, %2
-  %5 = sdiv i32 %4, %0
-  %6 = sdiv i32 %4, 10
-  %7 = mul nsw i32 1, %5
-  %8 = sdiv i32 %6, 128
-  %9 = sdiv i32 %7, 54
-  %10 = sdiv i32 %6, 1
-  %11 = sub nsw i32 %9, 0
+  %4 = sub i32 %3, 15
+  %5 = mul nsw i32 %4, %2
+  %6 = sdiv i32 %5, %0
+  %7 = sdiv i32 %5, 10
+  %8 = shl i32 %6, 0
+  %9 = sdiv i32 %7, 128
+  %10 = sdiv i32 %8, 54
+  %11 = sdiv i32 %7, 1
+  %12 = sub nsw i32 %10, 0
   ret void
 }
