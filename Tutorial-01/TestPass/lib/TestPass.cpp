7a1a4a8792ed4a4ab0e6cb716e179125cdb321cd
Manuel Pelloni <manuel.pelloni@m4ss.net>
2023-03-29 21:38:03 +0200

    reformat code

--- Tutorial-01/TestPass/lib/TestPass.cpp
+++ Tutorial-01/TestPass/lib/TestPass.cpp
@@ -12,30 +12,30 @@ public:
     outs() << "Passo di test per il corso di Linguaggi e Compilatori"
            << "\n";
 
-      // TODO: Completare il metodo come indicato per il LAB1.
-      for(auto iterM = M.begin(); iterM != M.end(); ++iterM){
-        Function &F = *iterM;
-        outs()<<F.getName()<<"\n";
-        int contBB=0,contCall=0;
-        //Conteggio basic blocks
-        for(auto iterF = F.begin(); iterF != F.end(); ++iterF){
-          BasicBlock &BB = *iterF;
-          contBB++;
-          for(auto iterBB = BB.begin(); iterBB != BB.end(); ++iterBB){
-            Instruction &Inst = *iterBB;
-            if(CallInst *call_inst=dyn_cast<CallInst>(&Inst))
-              contCall++;
-          }
+    // TODO: Completare il metodo come indicato per il LAB1.
+    for (auto iterM = M.begin(); iterM != M.end(); ++iterM) {
+      Function &F = *iterM;
+      outs() << F.getName() << "\n";
+      int contBB = 0, contCall = 0;
+      // Conteggio basic blocks
+      for (auto iterF = F.begin(); iterF != F.end(); ++iterF) {
+        BasicBlock &BB = *iterF;
+        contBB++;
+        for (auto iterBB = BB.begin(); iterBB != BB.end(); ++iterBB) {
+          Instruction &Inst = *iterBB;
+          if (CallInst *call_inst = dyn_cast<CallInst>(&Inst))
+            contCall++;
         }
-        outs()<<"\tNumero Basic Blocks: "<<contBB;
-        if(F.isVarArg()){
-          outs()<<"\n\tNumero Argomenti: "<<F.arg_size()<<"+*";
-        }else{
-          outs()<<"\n\tNumero Argomenti: "<<F.arg_size();
-        }
-        outs()<<"\n\tNumero Istruzioni: "<<F.getInstructionCount();
-        outs()<<"\n\tFunzioni Chiamate: "<<contCall<<"\n\n";
       }
+      outs() << "\tNumero Basic Blocks: " << contBB;
+      if (F.isVarArg()) {
+        outs() << "\n\tNumero Argomenti: " << F.arg_size() << "+*";
+      } else {
+        outs() << "\n\tNumero Argomenti: " << F.arg_size();
+      }
+      outs() << "\n\tNumero Istruzioni: " << F.getInstructionCount();
+      outs() << "\n\tFunzioni Chiamate: " << contCall << "\n\n";
+    }
 
     return PreservedAnalyses::all();
   }
@@ -44,12 +44,10 @@ public:
 } // anonymous namespace
 
 extern "C" PassPluginLibraryInfo llvmGetPassPluginInfo() {
-  return {
-      .APIVersion = LLVM_PLUGIN_API_VERSION,
-      .PluginName = "TestPass",
-      .PluginVersion = LLVM_VERSION_STRING,
-      .RegisterPassBuilderCallbacks =
-          [](PassBuilder &PB) {
+  return {.APIVersion = LLVM_PLUGIN_API_VERSION,
+          .PluginName = "TestPass",
+          .PluginVersion = LLVM_VERSION_STRING,
+          .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
             PB.registerPipelineParsingCallback(
                 [](StringRef Name, ModulePassManager &MPM,
                    ArrayRef<PassBuilder::PipelineElement>) -> bool {
@@ -59,6 +57,5 @@ extern "C" PassPluginLibraryInfo llvmGetPassPluginInfo() {
                   }
                   return false;
                 });
-          }
-  };
+          }};
 }
