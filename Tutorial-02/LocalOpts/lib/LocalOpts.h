7b0e0e890cac5a89a88ce805d9f30eed20b248cf
Manuel Pelloni <manuel.pelloni@m4ss.net>
2023-03-29 21:46:43 +0200

    format code

--- Tutorial-02/LocalOpts/lib/LocalOpts.h
+++ Tutorial-02/LocalOpts/lib/LocalOpts.h
@@ -1,18 +1,14 @@
 #pragma once // NOLINT(llvm-header-guard)
 
-#include <llvm/IR/PassManager.h>
 #include <llvm/IR/Constants.h>
+#include <llvm/IR/PassManager.h>
 
-class TransformPass final
-    : public llvm::PassInfoMixin<TransformPass> {
+class TransformPass final : public llvm::PassInfoMixin<TransformPass> {
 public:
-  llvm::PreservedAnalyses run(llvm::Module &,
-                              llvm::ModuleAnalysisManager &);
+  llvm::PreservedAnalyses run(llvm::Module &, llvm::ModuleAnalysisManager &);
 }; // class AlgebraicIdentityPass
 
-class AlgebraicIdentityPass final
-    : public llvm::PassInfoMixin<TransformPass> {
+class AlgebraicIdentityPass final : public llvm::PassInfoMixin<TransformPass> {
 public:
-  llvm::PreservedAnalyses run(llvm::Module &,
-                              llvm::ModuleAnalysisManager &);
+  llvm::PreservedAnalyses run(llvm::Module &, llvm::ModuleAnalysisManager &);
 };
