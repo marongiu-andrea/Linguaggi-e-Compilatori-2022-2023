1269f26724a25109cca456dec70c1430128a975b
Alex <ferrarini.alex24@gmail.com>
2023-03-29 21:08:02 +0200

    ultimated point 1/3.

--- Tutorial-02/LocalOpts/lib/AlgebraicIdentity.cpp
+++ Tutorial-02/LocalOpts/lib/AlgebraicIdentity.cpp
@@ -5,44 +5,40 @@ using namespace llvm;
 
 bool runOnBasicBlock2(BasicBlock &B) {
 
-  for(auto Iter=B.begin(); Iter != B.end(); ++Iter){
-      Instruction &Inst = *Iter;
-      if(Inst.getOpcode()==Instruction::Add){
-          ConstantInt *CI=dyn_cast<ConstantInt>(Inst.getOperand(1));
-           outs()<<"ADDIZIONE | ";
-           if(CI && (CI->getValue()).isZero()){
-                outs()<<"ADDIZIONE NULLA\n";
-                Inst.replaceAllUsesWith(Inst.getOperand(0));
-            } 
-        }
-      if(Inst.getOpcode()==Instruction::Mul){
-        ConstantInt *c1 = dyn_cast<ConstantInt>(Inst.getOperand(0));
-        ConstantInt *c2 = dyn_cast<ConstantInt>(Inst.getOperand(1));
-        if (c1 && c1->getValue().isOneValue())
-          Inst.replaceAllUsesWith(Inst.getOperand(1));    
-        else if (c2 && c2->getValue().isOneValue())
-          Inst.replaceAllUsesWith(Inst.getOperand(0));   
-      }
-  }
-
-    return true;
-  }
-
-
-  bool runOnFunction2(Function &F) {
-    bool Transformed = false;
-
-    for (auto Iter = F.begin(); Iter != F.end(); ++Iter) {
-      if (runOnBasicBlock2(*Iter)) {
-        Transformed = true;
+  for (auto Iter = B.begin(); Iter != B.end(); ++Iter) {
+    Instruction &Inst = *Iter;
+    if (Inst.getOpcode() == Instruction::Add) {
+      ConstantInt *CI = dyn_cast<ConstantInt>(Inst.getOperand(1));
+      outs() << "ADDIZIONE | ";
+      if (CI && (CI->getValue()).isZero()) {
+        outs() << "ADDIZIONE NULLA\n";
+        Inst.replaceAllUsesWith(Inst.getOperand(0));
       }
     }
-
-    return Transformed;
+    if (Inst.getOpcode() == Instruction::Mul) {
+      ConstantInt *c1 = dyn_cast<ConstantInt>(Inst.getOperand(0));
+      ConstantInt *c2 = dyn_cast<ConstantInt>(Inst.getOperand(1));
+      if (c1 && c1->getValue().isOneValue())
+        Inst.replaceAllUsesWith(Inst.getOperand(1));
+      else if (c2 && c2->getValue().isOneValue())
+        Inst.replaceAllUsesWith(Inst.getOperand(0));
+    }
   }
 
+  return true;
+}
 
+bool runOnFunction2(Function &F) {
+  bool Transformed = false;
 
+  for (auto Iter = F.begin(); Iter != F.end(); ++Iter) {
+    if (runOnBasicBlock2(*Iter)) {
+      Transformed = true;
+    }
+  }
+
+  return Transformed;
+}
 
 PreservedAnalyses AlgebraicIdentityPass::run([[maybe_unused]] Module &M,
                                              ModuleAnalysisManager &) {
@@ -56,4 +52,3 @@ PreservedAnalyses AlgebraicIdentityPass::run([[maybe_unused]] Module &M,
 
   return PreservedAnalyses::none();
 }
-
