89aed7d24023dd02dda283e31ba738ea9daf0dcb
Simone Bondi <simonebondi456@gmail.com>
2023-04-03 20:39:01 +0200

    MultiInstructionPass whitespace fixes Makefile passes string updated

--- Tutorial-02/LocalOpts/lib/MultiInstructionOperationsPass.cpp
+++ Tutorial-02/LocalOpts/lib/MultiInstructionOperationsPass.cpp
@@ -6,81 +6,85 @@ using namespace llvm;
  * controls left operand of instruction passed and replace usee of passed instruction with operand 1 of usee operands if both instructions
  * have same constant but different operation 
 */
-bool checkUseeInstruction(Value *opL, Value *opR,Instruction &inst,unsigned int opcode){
-     BinaryOperator *opLbinaryIns = dyn_cast<BinaryOperator>(opL);
-     if(opLbinaryIns){  
-     unsigned int opcodeopL = opLbinaryIns->getOpcode();
-  
-     if((opcode == Instruction::Add) && (opcodeopL == Instruction::Sub) ){
-         if(opR == dyn_cast<Value>(opLbinaryIns->getOperand(1))){
-          inst.replaceAllUsesWith(opLbinaryIns->getOperand(0));
-          
-           return true; 
-        }
-     }else if ((opcode == Instruction::Sub) && (opcodeopL == Instruction::Add)){
-        if(opR == dyn_cast<Value>(opLbinaryIns->getOperand(1))){
-          inst.replaceAllUsesWith(opLbinaryIns->getOperand(0));
-           return true;
-      }
-     }
-    }   
+bool checkUseeInstruction(Value *opL, Value *opR, Instruction &inst, unsigned int opcode){
+  BinaryOperator *opLbinaryIns = dyn_cast<BinaryOperator>(opL);
+  if (opLbinaryIns) {
+    unsigned int opcodeopL = opLbinaryIns->getOpcode();
 
-    
-     return false;
+    if ((opcode == Instruction::Add) && (opcodeopL == Instruction::Sub)) {
+      if (opR == dyn_cast<Value>(opLbinaryIns->getOperand(1))) {
+        inst.replaceAllUsesWith(opLbinaryIns->getOperand(0));
+        
+        return true;
+      }
+    }
+    else if ((opcode == Instruction::Sub) && (opcodeopL == Instruction::Add)) {
+      if (opR == dyn_cast<Value>(opLbinaryIns->getOperand(1))) {
+        inst.replaceAllUsesWith(opLbinaryIns->getOperand(0));
+
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
+
 /**
- * this function performs iteration on instrucions of basic block , checks instruction if it is addition or subtraction
+ * this function performs iteration on instructions of basic block, checks instruction if it is addition or subtraction
  *  
 */
 bool runOnBasicBlockMulti(BasicBlock &B){
-     std::vector<Instruction*> instRemove ;
-      for (auto &inst : B){
-        if(inst.isBinaryOp()){
-           bool remove_inst = false ;
-           Value *opL = inst.getOperand(0);
-           Value *opR = inst.getOperand(1);
-           unsigned int opcode = inst.getOpcode();
-           if(opcode == Instruction::Add){
-             remove_inst = checkUseeInstruction(opL,opR,inst,opcode);
-           }else if(opcode == Instruction::Sub){
-             remove_inst = checkUseeInstruction(opL,opR,inst,opcode);
-           }
-           if(remove_inst){
-             instRemove.push_back(&inst);
-           }
+  std::vector<Instruction*> instRemove;
 
-        }
+  for (auto &inst : B) {
+    if (inst.isBinaryOp()) {
+      bool remove_inst = false;
+      Value *opL = inst.getOperand(0);
+      Value *opR = inst.getOperand(1);
+      unsigned int opcode = inst.getOpcode();
+
+      if (opcode == Instruction::Add) {
+        remove_inst = checkUseeInstruction(opL, opR, inst, opcode);
+      } else if (opcode == Instruction::Sub) {
+        remove_inst = checkUseeInstruction(opL, opR, inst, opcode);
       }
-     
-     for (auto ins : instRemove){
-      ins->eraseFromParent();
-     }
+
+      if (remove_inst) {
+        instRemove.push_back(&inst);
+      }
+    }
+  }
+
+  for (auto ins : instRemove) {
+    ins->eraseFromParent();
+  }
 
   return true;
-     
 }
-
-
 
 bool runOnFunctionMulti(Function &F){
   bool Transformed = false;
 
-    for (auto Iter = F.begin(); Iter != F.end(); ++Iter) {
-      if (runOnBasicBlockMulti(*Iter)) {
-        Transformed = true;
-      }
+  for (auto Iter = F.begin(); Iter != F.end(); ++Iter) {
+    if (runOnBasicBlockMulti(*Iter)) {
+      Transformed = true;
     }
+  }
 
-    return Transformed;
+  return Transformed;
 }
 
 PreservedAnalyses MultiInstructionOperationsPass::run([[maybe_unused]] Module &M,
                                              ModuleAnalysisManager &) {
+
+  outs() << "MultiInstructionOperations\n";
+
   for (auto Iter = M.begin(); Iter != M.end(); ++Iter) {
     if (runOnFunctionMulti(*Iter)) {
       return PreservedAnalyses::none();
     }
   }
-  outs() << "MultiInstructionOperations\n";
+  
   return PreservedAnalyses::none();
 }
