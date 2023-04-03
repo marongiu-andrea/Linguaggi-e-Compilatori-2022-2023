958a04cba07d38824b7af10da55da62c5e7adfc7
syedjawad5 <naqvi6543@gmail.com>
2023-04-03 19:07:07 +0200

    removed segmentation fault and tested multi instruction pass

--- Tutorial-02/LocalOpts/lib/MultiInstructionOperationsPass.cpp
+++ Tutorial-02/LocalOpts/lib/MultiInstructionOperationsPass.cpp
@@ -2,13 +2,37 @@
 #include "llvm/IR/InstrTypes.h"
 using namespace llvm;
 
+/**
+ * controls left operand of instruction passed and replace usee of passed instruction with operand 1 of usee operands if both instructions
+ * have same constant but different operation 
+*/
+bool checkUseeInstruction(Value *opL, Value *opR,Instruction &inst,unsigned int opcode){
+     BinaryOperator *opLbinaryIns = dyn_cast<BinaryOperator>(opL);
+     if(opLbinaryIns){  
+     unsigned int opcodeopL = opLbinaryIns->getOpcode();
+  
+     if((opcode == Instruction::Add) && (opcodeopL == Instruction::Sub) ){
+         if(opR == dyn_cast<Value>(opLbinaryIns->getOperand(1))){
+          inst.replaceAllUsesWith(opLbinaryIns->getOperand(0));
+          
+           return true; 
+        }
+     }else if ((opcode == Instruction::Sub) && (opcodeopL == Instruction::Add)){
+        if(opR == dyn_cast<Value>(opLbinaryIns->getOperand(1))){
+          inst.replaceAllUsesWith(opLbinaryIns->getOperand(0));
+           return true;
+      }
+     }
+    }   
 
-
+    
+     return false;
+}
 /**
  * this function performs iteration on instrucions of basic block , checks instruction if it is addition or subtraction
  *  
 */
-bool runOnBasicBlock(BasicBlock &B){
+bool runOnBasicBlockMulti(BasicBlock &B){
      std::vector<Instruction*> instRemove ;
       for (auto &inst : B){
         if(inst.isBinaryOp()){
@@ -28,42 +52,21 @@ bool runOnBasicBlock(BasicBlock &B){
         }
       }
      
-     for (auto ins : instRemove) 
+     for (auto ins : instRemove){
       ins->eraseFromParent();
+     }
 
   return true;
      
 }
 
-/**
- * controls left operand of instruction passed and replace usee of passed instruction with operand 1 of usee operands if both instructions
- * have same constant but different operation 
-*/
-bool checkUseeInstruction(Value *opL, Value *opR,Instruction &inst,unsigned int opcode){
-     BinaryOperator *opLbinaryIns = dyn_cast<BinaryOperator>(opL);
-     unsigned int opcodeopL = opLbinaryIns->getOpcode();
-     if(opLbinaryIns){
-     if((opcode == Instruction::Add) && (opcodeopL == Instruction::Sub) ){
-        if(opR == dyn_cast<Value>(opLbinaryIns->getOperand(1))){
-          inst.replaceAllUsesWith(opLbinaryIns->getOperand(0));
-           return true;
-        }
-     }else if ((opcode == Instruction::Sub) && (opcodeopL == Instruction::Add)){
-        if(opR == dyn_cast<Value>(opLbinaryIns->getOperand(1))){
-          inst.replaceAllUsesWith(opLbinaryIns->getOperand(0));
-           return true;
-      }
-     }
-     } 
 
-     return false;
-}
 
-bool runOnFunction(Function &F){
+bool runOnFunctionMulti(Function &F){
   bool Transformed = false;
 
     for (auto Iter = F.begin(); Iter != F.end(); ++Iter) {
-      if (runOnBasicBlock(*Iter)) {
+      if (runOnBasicBlockMulti(*Iter)) {
         Transformed = true;
       }
     }
@@ -74,7 +77,7 @@ bool runOnFunction(Function &F){
 PreservedAnalyses MultiInstructionOperationsPass::run([[maybe_unused]] Module &M,
                                              ModuleAnalysisManager &) {
   for (auto Iter = M.begin(); Iter != M.end(); ++Iter) {
-    if (runOnFunction(*Iter)) {
+    if (runOnFunctionMulti(*Iter)) {
       return PreservedAnalyses::none();
     }
   }
