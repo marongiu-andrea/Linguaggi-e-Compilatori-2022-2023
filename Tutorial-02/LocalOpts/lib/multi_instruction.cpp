4bf1759baa78434787357d3d10edabfd7b62fe00
simone <simonevaiasinni@gmail.com>
2023-04-05 18:14:22 +0200

    Multi instruction optimization Completato!

--- Tutorial-02/LocalOpts/lib/multi_instruction.cpp
+++ Tutorial-02/LocalOpts/lib/multi_instruction.cpp
@@ -1,109 +1,106 @@
-// #include "LocalOpts.h"
-// #include "llvm/IR/InstrTypes.h"
-// #include <map>
+#include "LocalOpts.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/ADT/APInt.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/IRBuilder.h"
+#include <vector>
 
-// using namespace llvm;
+using namespace llvm;
 
-// std::map<uint32_t, uint32_t> mapping = {
-//   std::make_pair(Instruction::Add, Instruction::Sub),
-//   std::make_pair(Instruction::Sub, Instruction::Add)
-// };
+static std::vector<Instruction*> toRemove;
 
-// bool check(
-//   const ConstantInt*  first, 
-//   const ConstantInt*  second, 
-//   const uint32_t      type, 
-//   const Instruction*  instr) 
-// {
-//   if(instr->getOpcode() == type && first->getValue() == second->getValue())
-//     return true;
-  
-//   return false;
-// }
-
-// bool assign(ConstantInt*& constant, Value*& operand, Instruction* instr)
-// {
-//   ConstantInt* first = dyn_cast<ConstantInt>(instr->getOperand(0));
-//   ConstantInt* second = dyn_cast<ConstantInt>(instr->getOperand(1));
-
-//   if(first && second || !first && !second)
-//     return false;
-
-//   constant = second;
-//   operand = instr->getOperand(0);
-
-//   if(first) 
-//   {
-//     constant = first;
-//     operand = instr->getOperand(1);
-//   } 
-//   return true;
-// }
-
-// bool multiInstrOpt(BasicBlock& BB)
-// {
-//   for(BasicBlock::iterator itBB = BB.begin(); itBB != BB.end(); ++itBB) 
-//   {
-//     Instruction& instr = *itBB;
-//     const uint32_t opcode = instr.getOpcode();
+static bool multiInstructionOpt(BasicBlock& BB)
+{
+  for(Instruction& instruction : BB)
+  {
+    outs() << instruction << "\n";
+    if(instruction.getNumOperands() != 2) continue;
     
-//     ConstantInt*  firstConst;
-//     ConstantInt*  secondConst;
-//     Value*        firstInstOperand;
-//     Value*        secondInstOperand;
-
-//     if(mapping.find(opcode) == mapping.end())
-//       mapping.insert({opcode, 0});
-
-//     if(instr.isBinaryOp() && mapping.at(opcode)) 
-//     {
-//       if(!assign(firstConst, firstInstOperand, &instr))
-//         continue;
-
-//       for (auto itUser = instr.user_begin(); itUser != instr.user_end(); ++itUser) 
-//       {
-//         Instruction* user = dyn_cast<Instruction>(*itUser);
+    Value* op1 = instruction.getOperand(0);
+    Value* op2 = instruction.getOperand(1);
+    const ConstantInt* constVal2 = dyn_cast<ConstantInt>(op2);
+    
+    const uint32_t opcode = instruction.getOpcode();
+    switch (opcode)
+    {
+      case Instruction::Add: {
+        // case <var> + <const>
+        if(constVal2)
+        {
+          BinaryOperator* binaryOp = dyn_cast<BinaryOperator>(op1);
+          if (binaryOp && binaryOp->getOpcode() == Instruction::Sub)
+          {
+            if (binaryOp->getOperand(1) == op2)
+            {
+              outs() << "\nInstruction to remove:" << instruction 
+                << "e rimpiazzo con " << *(binaryOp->getOperand(0)) << " \n";
+              
+              instruction.replaceAllUsesWith(binaryOp->getOperand(0));
+              toRemove.push_back(&instruction);
+            }
+          }
+        }
         
-//         if(user->isBinaryOp() && mapping.at(opcode)) 
-//         {
-//           if(!assign(secondConst, secondInstOperand, user))
-//             continue;
-          
-//           if(check(firstConst, secondConst, mapping.at(opcode), user)) 
-//             user->replaceAllUsesWith(firstInstOperand);
-//         }
-//       }
-//     }
+        break;
+      }
 
-//   }
+      case Instruction::Sub: {
+        // case <var> - <const>
+        if(constVal2)
+        {
+          BinaryOperator* binaryOp = dyn_cast<BinaryOperator>(op1);
+          if (binaryOp && binaryOp->getOpcode() == Instruction::Add)
+          {
+            if (binaryOp->getOperand(1) == op2)
+            {
+              outs() << "\nInstruction to remove:" << instruction 
+                << "e rimpiazzo con " << *(binaryOp->getOperand(0)) << " \n";
+              
+              instruction.replaceAllUsesWith(binaryOp->getOperand(0));
+              toRemove.push_back(&instruction);
+            }
+          }
+        }
+        
+        break;
+      }
+    }
 
-//   return true;
-// }
+    outs() << "\n ---------------- \n";
+  }
 
-// static bool runOnFunction(Function& F) 
-// {
-//   bool transformed = false;
+  for (Instruction* instruction : toRemove)
+    instruction->eraseFromParent();
 
-//   for (auto it = F.begin(); it != F.end(); ++it)
-//   {
-//     if (multiInstrOpt(*it)) 
-//     {
-//       transformed = true;
-//     } 
-//   }
-//   return transformed;
-// }
+  return true;
+}
 
-// PreservedAnalyses MultiInstructionPass::run([[maybe_unused]] Module &M,ModuleAnalysisManager &) 
-// {
-//   // Un semplice passo di esempio di manipolazione della IR
-//   for (auto it = M.begin(); it != M.end(); ++it) 
-//   {
-//     if (runOnFunction(*it))
-//     {
-//       return PreservedAnalyses::none();
-//     }
-//   }
 
-//   return PreservedAnalyses::none();
-// }
+static bool runOnFunction(Function& F) 
+{
+  bool transformed = false;
+
+  for (auto it = F.begin(); it != F.end(); ++it)
+  {
+    if (multiInstructionOpt(*it)) 
+    {
+      transformed = true;
+    } 
+  }
+  return transformed;
+}
+
+PreservedAnalyses MultiInstructionPass::run([[maybe_unused]] Module &M,ModuleAnalysisManager &) 
+{
+  // Un semplice passo di esempio di manipolazione della IR
+  for (auto it = M.begin(); it != M.end(); ++it) 
+  {
+    if (runOnFunction(*it))
+    {
+      return PreservedAnalyses::none();
+    }
+  }
+
+  return PreservedAnalyses::none();
+}
