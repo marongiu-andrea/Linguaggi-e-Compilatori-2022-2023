f4b25e257b2abcc45d34a44052681123353ebcda
Alex <ferrarini.alex24@gmail.com>
2023-04-07 16:58:14 +0200

    update  point 2/3, deletion still needs to be implemented.

--- Tutorial-02/LocalOpts/lib/StrengthReduction.cpp
+++ Tutorial-02/LocalOpts/lib/StrengthReduction.cpp
@@ -1,7 +1,12 @@
 #include "LocalOpts.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Type.h"
 #include "llvm/IR/Value.h"
+#include "llvm/Support/Casting.h"
 #include "llvm/Support/MathExtras.h"
+#include "llvm/Support/raw_ostream.h"
 #include <cmath>
 #include <cstdint>
 #include <math.h>
@@ -17,20 +22,22 @@ bool runOnBasicBlock3(BasicBlock &B) {
       ConstantInt *c2 = dyn_cast<ConstantInt>(Inst.getOperand(1));
       if (c1) {
         if ((c1->getValue()).logBase2()) {
-          outs()<<"Potenza Rilevata\n";
-          Value *NewInst = BinaryOperator::CreateShl(c2, c1,"",&Inst);
+          outs()<<"Potenza Rilevata c1\n";
+          Value *c2=Inst.getOperand(1);
+          Value *NewInst = BinaryOperator::CreateShl(c2, ConstantInt::get(c1->getType(),c1->getValue().logBase2()),"",&Inst);
           Inst.replaceAllUsesWith(NewInst);
-          Inst.removeFromParent();
+          //Inst.removeFromParent();
         } else {
           // todo
         }
 
       } else if (c2) {
         if ((c2->getValue()).logBase2()) {
-         outs()<<"Potenza Rilevata\n";
-          Value *NewInst = BinaryOperator::CreateShl(c1, c2,"");
-          Inst.replaceAllUsesWith(NewInst);
-          Inst.removeFromParent();
+         outs()<<"Potenza Rilevata c2\n";
+         Value *c1=Inst.getOperand(0);
+         Value *NewInst = BinaryOperator::CreateShl(c1, ConstantInt::get(c2->getType(),c2->getValue().logBase2()),"",&Inst);
+         Inst.replaceAllUsesWith(NewInst);
+         //Inst.removeFromParent();//Probabilnmente non da usare in ottimizzazione finale
         }else{
             //todo
         }
--- Tutorial-02/LocalOpts/test/test.optSR.ll
+++ Tutorial-02/LocalOpts/test/test.optSR.ll
@@ -3,14 +3,15 @@ source_filename = "test/test.ll"
 
 define dso_local void @foo(i32 noundef %0) {
   %2 = add nsw i32 %0, 0
-  %3 = mul nsw i32 %2, 16
-  %4 = mul nsw i32 %3, %2
-  %5 = sdiv i32 %4, %0
-  %6 = sdiv i32 %4, 10
-  %7 = mul nsw i32 1, %5
-  %8 = sdiv i32 %6, 128
-  %9 = sdiv i32 %7, 54
-  %10 = sdiv i32 %6, 1
-  %11 = sub nsw i32 %9, 0
+  %3 = shl i32 %2, 4
+  %4 = mul nsw i32 %2, 16
+  %5 = mul nsw i32 %3, %2
+  %6 = sdiv i32 %5, %0
+  %7 = sdiv i32 %5, 10
+  %8 = mul nsw i32 1, %6
+  %9 = sdiv i32 %7, 128
+  %10 = sdiv i32 %8, 54
+  %11 = sdiv i32 %7, 1
+  %12 = sub nsw i32 %10, 0
   ret void
 }
