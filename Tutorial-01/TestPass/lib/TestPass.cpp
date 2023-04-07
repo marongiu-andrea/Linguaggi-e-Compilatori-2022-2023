173fe137f0c4670913b01fcf2e9dbc180449fd0d
Gamescrash <pallone2002@gmail.com>
2023-04-07 19:21:19 +0200

    07/04

--- Tutorial-01/TestPass/lib/TestPass.cpp
+++ Tutorial-01/TestPass/lib/TestPass.cpp
@@ -4,53 +4,81 @@
 
 using namespace llvm;
 
-namespace {
+namespace
+{
 
-class TestPass final : public PassInfoMixin<TestPass> {
-public:
-  PreservedAnalyses run([[maybe_unused]] Module &M, ModuleAnalysisManager &) {
-    outs() << "Passo di test per il corso di Linguaggi e Compilatori"
-           << "\n";
+  class TestPass final : public PassInfoMixin<TestPass>
+  {
+  public:
+    PreservedAnalyses run([[maybe_unused]] Module &M, ModuleAnalysisManager &)
+    {
+      outs() << "Passo di test per il corso di Linguaggi e Compilatori"
+             << "\n\n";
 
-      // TODO: Completare il metodo come indicato per il LAB1.
-      for (auto iter = M.begin(); iter != M.end(); ++iter){
-        int bbcount = 0;
-        int instcount = 0;
-        int callsitescount = 0;
+      for (auto iter = M.begin(); iter != M.end(); iter++)
+      {
+        auto &function = *iter;
 
-        Function &F = iter;
-        outs() <<"\nFunction Name: " << F.getName() << "\n";
+        // Nome funzione
+        outs() << "Nome funzione: " << function.getName() << "\n";
+        auto functionType = function.getFunctionType();
 
-#if 0
-        Value &V = 'iter';
+        // Numero parametri funzione
+        outs() << "Numero parametri: " << functionType->getNumParams();
+        if (functionType->isVarArg())
+          outs() << "+*";
+        outs() << "\n";
 
-        for (auto iter v)
+        // Numero di chiamate ad altre funzioni
+        uint callCount = 0;
+        uint bbCount = 0;
+        uint instructionCount = 0;
+
+        for (auto &bb : function)
+        {
+          bbCount++;
+          for (auto &inst : bb)
+          {
+            if (dyn_cast<CallBase>(&inst) != nullptr)
+              callCount++;
+
+            instructionCount++;
+          }
+        }
+        outs() << "Numero chiamate a funzioni: " << callCount << "\n";
+
+        // Numero di Basic Blocks
+        outs() << "Numero di BB: " << bbCount << "\n";
+
+        // Numero di Instructions
+        outs() << "Numero di Instructions: " << instructionCount << "\n";
+
+        outs() << "\n";
       }
-    
-    
-
-    return PreservedAnalyses::all();
-  }
-}; // class TestPass
+      return PreservedAnalyses::all();
+    }
+  }; // class TestPass
 
 } // anonymous namespace
-
-extern "C" PassPluginLibraryInfo llvmGetPassPluginInfo() {
+extern "C" PassPluginLibraryInfo llvmGetPassPluginInfo()
+{
   return {
       .APIVersion = LLVM_PLUGIN_API_VERSION,
       .PluginName = "TestPass",
       .PluginVersion = LLVM_VERSION_STRING,
       .RegisterPassBuilderCallbacks =
-          [](PassBuilder &PB) {
-            PB.registerPipelineParsingCallback(
-                [](StringRef Name, ModulePassManager &MPM,
-                   ArrayRef<PassBuilder::PipelineElement>) -> bool {
-                  if (Name == "test-pass") {
-                    MPM.addPass(TestPass());
-                    return true;
-                  }
-                  return false;
-                });
-          } 
-  };
+          [](PassBuilder &PB)
+      {
+        PB.registerPipelineParsingCallback(
+            [](StringRef Name, ModulePassManager &MPM,
+               ArrayRef<PassBuilder::PipelineElement>) -> bool
+            {
+              if (Name == "test-pass")
+              {
+                MPM.addPass(TestPass());
+                return true;
+              }
+              return false;
+            });
+      }};
 }
