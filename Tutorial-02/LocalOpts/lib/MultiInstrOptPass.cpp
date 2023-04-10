9aa4e82265dfeb1e650d584940feb7e699e3f7a3
Riccardo Cracco <281925@studenti.unimore.it>
2023-04-10 15:53:10 +0200

    Fix e refactoring.

--- Tutorial-02/LocalOpts/lib/MultiInstrOptPass.cpp
+++ Tutorial-02/LocalOpts/lib/MultiInstrOptPass.cpp
@@ -1,39 +1,44 @@
 #include "LocalOpts.h"
 #include "llvm/IR/InstrTypes.h"
 #include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/Transforms/Utils/BasicBlockUtils.h>
+#include <utility>
 
 using namespace llvm;
 
+static std::pair<Instruction*, Instruction*> getOperandsAsInstruction(Instruction& instr)
+{
+    return {dyn_cast<Instruction>(instr.getOperand(0)), dyn_cast<Instruction>(instr.getOperand(1))};
+}
+
 /*
- * Function that tries to apply a multi-instruction optimization to an addition in input and returns true if it succeeds.
+ * Function that tries to apply a multi-instruction optimization to an addition in input and returns the value to use to replace that instruction if it succeeds.
  * First the function tries to convert the operands of the addition into instructions. Then it checks the first operand to
  * see if it is a subtraction and if its second operand is equal to the second operand of the input instruction. If all the
  * conditions are met, the function replaces all the uses of the input instruction with the first operand of the subtraction.
  * Otherwise similar checks are repeated with the second operand of the input but this time it checks if the second operand
  * of the subtraction is equal to the first one of the addition and then it replaces all the uses with the first operand of the subtraction.
  */
-bool multi_op_add(Instruction& inst)
+Value* multi_op_add(Instruction& inst)
 {
-    Instruction* op0 = dyn_cast<Instruction>(inst.getOperand(0));
-    Instruction* op1 = dyn_cast<Instruction>(inst.getOperand(1));
+    auto [op0, op1] = getOperandsAsInstruction(inst);
 
     if (op0 && op0->getOpcode() == Instruction::Sub && op0->getOperand(1) == inst.getOperand(1))
     {
-        inst.replaceAllUsesWith(op0->getOperand(0));
-        return true;
+        return op0->getOperand(0);
     }
 
     if (op1 && op1->getOpcode() == Instruction::Sub && op1->getOperand(1) == inst.getOperand(0))
     {
-        inst.replaceAllUsesWith(op1->getOperand(0));
-        return true;
+        return op1->getOperand(0);
     }
 
-    return false;
+    return nullptr;
 }
 
 /*
- * Function that tries to apply a multi-instruction optimization to a subtraction in input and returns true if it succeeds.
+ * Function that tries to apply a multi-instruction optimization to a subtraction in input and returns the value to use to replace that instruction if it succeeds.
  * First the function tries to convert the operands of the subtraction into instructions. Then it checks the first operand to
  * see if it is an addition and if any of its operands are equal to the second operand of the subtraction. If all the conditions
  * are met, the uses of the input instruction are replaced with the operand of the addition that is different from the second
@@ -41,82 +46,71 @@ bool multi_op_add(Instruction& inst)
  * addition with any operand equal to the first operand of the input. In this case the input instruction is replaced with another
  * instruction that computes the opposite of the addition’s operand that is different from the first operand of the input.
  */
-bool multi_op_sub(Instruction& inst)
+Value* multi_op_sub(Instruction& inst)
 {
-    Instruction* op0 = dyn_cast<Instruction>(inst.getOperand(0));
-    Instruction* op1 = dyn_cast<Instruction>(inst.getOperand(1));
+    auto [op0, op1] = getOperandsAsInstruction(inst);
 
     if (op0 && op0->getOpcode() == Instruction::Add)
     {
 
         if (op0->getOperand(0) == inst.getOperand(1))
         {
-            inst.replaceAllUsesWith(op0->getOperand(1));
-            return true;
+            return op0->getOperand(1);
         }
 
         if (op0->getOperand(1) == inst.getOperand(1))
         {
-            inst.replaceAllUsesWith(op0->getOperand(0));
-            return true;
+            return op0->getOperand(0);
         }
     }
 
     if (op1 && op1->getOpcode() == Instruction::Add)
     {
-
         if (op1->getOperand(0) == inst.getOperand(0))
         {
-            llvm::Type* i64_type = llvm::IntegerType::getInt64Ty(inst.getContext());
-            Instruction* newinst = BinaryOperator::Create(Instruction::Sub, ConstantInt::get(i64_type, 0), op1->getOperand(1));
-            newinst->insertAfter(&inst);
-            inst.replaceAllUsesWith(newinst);
-            return true;
+            IRBuilder builder(&inst);
+
+            return builder.CreateSub(ConstantInt::get(op1->getType(), 0), op1->getOperand(1), "sub", op1->hasNoUnsignedWrap(), op1->hasNoSignedWrap());
         }
 
         if (op1->getOperand(1) == inst.getOperand(0))
         {
-            llvm::Type* i64_type = llvm::IntegerType::getInt64Ty(inst.getContext());
-            Instruction* newinst = BinaryOperator::Create(Instruction::Sub, ConstantInt::get(i64_type, 0), op1->getOperand(0));
-            newinst->insertAfter(&inst);
-            inst.replaceAllUsesWith(newinst);
-            return true;
+            IRBuilder builder(&inst);
+
+            return builder.CreateSub(ConstantInt::get(op1->getType(), 0), op1->getOperand(0), "sub", op1->hasNoUnsignedWrap(), op1->hasNoSignedWrap());
         }
     }
 
-    return false;
+    return nullptr;
 }
 
 /*
- * Function that tries to apply a multi-instruction optimization to a multiplication in input and returns true if it succeeds.
+ * Function that tries to apply a multi-instruction optimization to a multiplication in input and returns the value to use to replace that instruction if it succeeds.
  * First the function tries to convert the operands of the multiplication into instructions. Then it checks the first operand to
  * see if it is a division and if its second operand is equal to the second operand of the input instruction. If all the conditions
  * are met, the function replaces all the uses of the input instruction with the first operand of the division. Otherwise similar
  * checks are repeated with the second operand of the input but this time it checks if the second operand of the division is equal
  * to the first one of the multiplication and then it replaces all the uses with the first operand of the division.
  */
-bool multi_op_mul(Instruction& inst)
+Value* multi_op_mul(Instruction& inst)
 {
-    Instruction* op0 = dyn_cast<Instruction>(inst.getOperand(0));
-    Instruction* op1 = dyn_cast<Instruction>(inst.getOperand(1));
+    auto [op0, op1] = getOperandsAsInstruction(inst);
 
     if (op0 && (op0->getOpcode() == Instruction::SDiv || op0->getOpcode() == Instruction::UDiv) && op0->getOperand(1) == inst.getOperand(1))
     {
-        inst.replaceAllUsesWith(op0->getOperand(0));
-        return true;
+        return op0->getOperand(0);
     }
 
     if (op1 && (op1->getOpcode() == Instruction::SDiv || op1->getOpcode() == Instruction::UDiv) && op1->getOperand(1) == inst.getOperand(0))
     {
-        inst.replaceAllUsesWith(op1->getOperand(0));
-        return true;
+        return op1->getOperand(0);
     }
 
-    return false;
+    return nullptr;
 }
 
 /*
- * Function that tries to apply a multi-instruction optimization to a division in input and returns true if it succeeds.
+ * Function that tries to apply a multi-instruction optimization to a division in input and returns the value to use to replace that instruction if it succeeds.
  * First the function tries to convert the operands of the division into instructions. Then it checks the first operand to
  * see if it is a multiplication and if any of its operands are equal to the second operand of the division. If all the
  * conditions are met, the uses of the input instruction are replaced with the operand of the multiplication that is different
@@ -124,50 +118,41 @@ bool multi_op_mul(Instruction& inst)
  * the input is a multiplication with any operand equal to the first operand of the input. In this case the input instruction is
  * replaced with another instruction that computes the inverse of the multiplication’s operand that is different from the first operand of the input.
  */
-bool multi_op_div(Instruction& inst)
+Value* multi_op_div(Instruction& inst)
 {
-    Instruction* op0 = dyn_cast<Instruction>(inst.getOperand(0));
-    Instruction* op1 = dyn_cast<Instruction>(inst.getOperand(1));
+    auto [op0, op1] = getOperandsAsInstruction(inst);
 
     if (op0 && op0->getOpcode() == Instruction::Mul)
     {
-
         if (op0->getOperand(0) == inst.getOperand(1))
         {
-            inst.replaceAllUsesWith(op0->getOperand(1));
-            return true;
+            return op0->getOperand(1);
         }
 
         if (op0->getOperand(1) == inst.getOperand(1))
         {
-            inst.replaceAllUsesWith(op0->getOperand(0));
-            return true;
+            return op0->getOperand(0);
         }
     }
 
     if (op1 && op1->getOpcode() == Instruction::Mul)
     {
-
         if (op1->getOperand(0) == inst.getOperand(0))
         {
-            llvm::Type* i64_type = llvm::IntegerType::getInt64Ty(inst.getContext());
-            Instruction* newinst = BinaryOperator::Create(Instruction::SDiv, ConstantInt::get(i64_type, 1), op1->getOperand(1));
-            newinst->insertAfter(&inst);
-            inst.replaceAllUsesWith(newinst);
-            return true;
+            IRBuilder builder(&inst);
+
+            return builder.CreateSDiv(ConstantInt::get(op1->getType(), 1), op1->getOperand(1));
         }
 
         if (op1->getOperand(1) == inst.getOperand(0))
         {
-            llvm::Type* i64_type = llvm::IntegerType::getInt64Ty(inst.getContext());
-            Instruction* newinst = BinaryOperator::Create(Instruction::SDiv, ConstantInt::get(i64_type, 1), op1->getOperand(0));
-            newinst->insertAfter(&inst);
-            inst.replaceAllUsesWith(newinst);
-            return true;
+            IRBuilder builder(&inst);
+
+            return builder.CreateSDiv(ConstantInt::get(op1->getType(), 1), op1->getOperand(0));
         }
     }
 
-    return false;
+    return nullptr;
 }
 
 /*
@@ -178,57 +163,44 @@ bool multi_op_div(Instruction& inst)
 bool MultiInstrOptPass::runOnBasicBlock(llvm::BasicBlock& bb)
 {
     bool transformed = false;
-    SmallVector<Instruction*, 0> to_delete;
+    std::vector<std::pair<Instruction*, Value*>> instructionsToReplace;
 
     for (auto& inst : bb)
     {
+        Value* replacingValue = nullptr;
+
         switch (inst.getOpcode())
         {
             case Instruction::Add:
-                if (multi_op_add(inst))
-                {
-                    to_delete.push_back(&inst);
-                    transformed = true;
-                }
+                replacingValue = multi_op_add(inst);
                 break;
 
             case Instruction::Sub:
-                if (multi_op_sub(inst))
-                {
-                    to_delete.push_back(&inst);
-                    transformed = true;
-                }
+                replacingValue = multi_op_sub(inst);
                 break;
 
             case Instruction::Mul:
-                if (multi_op_mul(inst))
-                {
-                    to_delete.push_back(&inst);
-                    transformed = true;
-                }
+                replacingValue = multi_op_mul(inst);
                 break;
 
             case Instruction::SDiv:
-                if (multi_op_div(inst))
-                {
-                    to_delete.push_back(&inst);
-                    transformed = true;
-                }
-                break;
-
             case Instruction::UDiv:
-                if (multi_op_add(inst))
-                {
-                    to_delete.push_back(&inst);
-                    transformed = true;
-                }
+                replacingValue = multi_op_div(inst);
                 break;
+        }
+
+        if (replacingValue != nullptr)
+        {
+            instructionsToReplace.emplace_back(&inst, replacingValue);
         }
     }
 
-    for (auto* instr : to_delete)
+    for (auto [instructionToReplace, replacingValue] : instructionsToReplace)
     {
-        instr->eraseFromParent();
+        BasicBlock::iterator it(instructionToReplace);
+
+        ReplaceInstWithValue(bb.getInstList(), it, replacingValue);
+        transformed = true;
     }
 
     return transformed;
