619cb4c8103665700c7e81f44994607b87b23fa1
Nickbarbieri1 <barbieri.nic01@gmail.com>
2023-04-13 17:26:57 +0200

    Completato il primo punto dell'esercizio sui loop

--- Tutorial-03/lib/LoopWalk.cpp
+++ Tutorial-03/lib/LoopWalk.cpp
@@ -16,7 +16,28 @@ public:
 
   virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override {
     outs() << "\nLOOPPASS INIZIATO...\n"; 
-    return false; 
+    //verifico se il loop e' nella forma normale/semplificata
+    if (L->isLoopSimplifyForm()) {
+      outs() << "Il Loop e' nella forma normale!\n\n";
+    }
+    //prendo il blocco di preheader e lo stampo
+    BasicBlock *preheader = L->getLoopPreheader();
+    if (preheader){
+      outs() << "Il loop contiene un blocco di preheader!\n";
+      preheader->print(outs());
+      outs() << "\n";
+    }
+    //scorro tutti i BB che compongono il loop e li stampo
+    BasicBlock *actualBB;
+    outs() << "Procedo alla stampa dei BB che compongono il loop:\n\n";
+    for (Loop::block_iterator BI = L->block_begin(); BI != L->block_end(); ++BI) {
+      actualBB = *BI;
+      if(actualBB) {
+        actualBB->print(outs());
+        outs() << "\n";
+      }
+    }
+    return false;
   }
 };
 
