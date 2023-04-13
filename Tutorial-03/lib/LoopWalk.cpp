f0386ee1956fc9bc27919f60e118f79c5a734f3e
283186 <283186@studenti.unimore.it>
2023-04-13 17:29:16 +0200

    LoopWalkPass esercizio-1

--- Tutorial-03/lib/LoopWalk.cpp
+++ Tutorial-03/lib/LoopWalk.cpp
@@ -16,6 +16,27 @@ public:
 
   virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override {
     outs() << "\nLOOPPASS INIZIATO...\n"; 
+
+    // verificare la forma normalizzata
+    if (L->isLoopSimplifyForm()){
+      outs() << "\nLoop in forma normalizzata\n";
+      
+      // controllo preheader
+      BasicBlock *B1 = L->getLoopPreheader();
+      outs() << "\nPreheader del loop: " <<  *B1 << "\n";
+
+      // stampo i basic blocks del loop
+      int i = 1;
+      for (Loop::block_iterator BI = L->block_begin(); BI != L->block_end(); ++BI){
+
+        BasicBlock *BB = *BI;
+
+        outs() << "\nBasic block n. " << i << ": " << *BB << "\n";
+        i++;
+      }
+
+    }
+    
     return false; 
   }
 };
