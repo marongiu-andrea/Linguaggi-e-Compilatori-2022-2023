d84cd6337b5fc2b4c95305cdff96bdfe2a63e3f8
Alex <ferrarini.alex24@gmail.com>
2023-04-13 18:01:09 +0200

    Tutorial-03 da terminare ultimo punto

--- Tutorial-03/lib/LoopWalk.cpp
+++ Tutorial-03/lib/LoopWalk.cpp
@@ -1,3 +1,9 @@
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Value.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/raw_ostream.h"
 #include <llvm/Analysis/LoopPass.h>
 #include <llvm/Analysis/ValueTracking.h>
 
@@ -11,18 +17,50 @@ public:
 
   LoopWalkPass() : LoopPass(ID) {}
 
-  virtual void getAnalysisUsage(AnalysisUsage &AU) const override {
-  }
+  virtual void getAnalysisUsage(AnalysisUsage &AU) const override {}
 
   virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override {
-    outs() << "\nLOOPPASS INIZIATO...\n"; 
-    return false; 
+    outs() << "\nLOOPPASS INIZIATO...\n";
+    if (L->isLoopSimplifyForm()) {
+      if (L->getLoopPreheader()) {
+        BasicBlock *BB = L->getLoopPreheader();
+        outs() << *BB << "\n";
+
+        for (Loop::block_iterator BI = L->block_begin(); BI != L->block_end();
+             ++BI) {
+          BasicBlock *B = *BI;
+          outs() << *B << "\n";
+        }
+        for (Loop::block_iterator BI = L->block_begin(); BI != L->block_end();
+             ++BI) {
+          BasicBlock *B = *BI;
+          for (auto iter = B->begin(); iter != B->end(); ++iter) {
+            Instruction &inst = *iter;
+            if (inst.getOpcode() == Instruction::Sub) {
+              ConstantInt *c1 = dyn_cast<ConstantInt>(inst.getOperand(0));
+              ConstantInt *c2 = dyn_cast<ConstantInt>(inst.getOperand(1));
+              if (!c1) {
+                Value *opVal = inst.getOperand(0);
+                Instruction *def = dyn_cast<Instruction>(opVal);
+                outs() << "BasicBlock della definizione dell' istruzione sub:\n"
+                       << *B;
+              }
+              if (!c2) {
+                Value *opVal = inst.getOperand(1);
+                Instruction *def = dyn_cast<Instruction>(opVal);
+                outs() << "BasicBlock della definizione dell' istruzione sub:\n"
+                       << *B;
+              }
+            }
+          }
+        }
+      }
+    }
+    return false;
   }
 };
 
 char LoopWalkPass::ID = 0;
-RegisterPass<LoopWalkPass> X("loop-walk",
-                             "Loop Walk");
+RegisterPass<LoopWalkPass> X("loop-walk", "Loop Walk");
 
 } // anonymous namespace
-
