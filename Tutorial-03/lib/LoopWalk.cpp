41891327dda0580ebe9df93197b011c4501bbc46
Raul Pagano <paganoraul02@gmail.com>
2023-04-13 17:54:55 +0200

    primo es primo punto

--- Tutorial-03/lib/LoopWalk.cpp
+++ Tutorial-03/lib/LoopWalk.cpp
@@ -1,5 +1,6 @@
 #include <llvm/Analysis/LoopPass.h>
 #include <llvm/Analysis/ValueTracking.h>
+#include <llvm/Analysis/LoopInfo.h>
 
 using namespace llvm;
 
@@ -16,9 +17,21 @@ public:
 
   virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override {
     outs() << "\nLOOPPASS INIZIATO...\n"; 
+    if (L->isLoopSimplifyForm())
+        outs() << "true\n";
+      else
+        outs() << "false\n";
+    
+    
+    BasicBlock* preheder = L->getLoopPreheader();
+    // LoopPass* simplify = isLoopSimplifyForm();
+    if(preheder){
+      outs() << "Il loop ha un preheader:\nIstruzioni Preheader\n";
+    }  
     return false; 
   }
 };
+
 
 char LoopWalkPass::ID = 0;
 RegisterPass<LoopWalkPass> X("loop-walk",
