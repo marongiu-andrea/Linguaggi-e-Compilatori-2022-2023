e934f889cca5dc2cf7c233823b4dc4bda00cc9d3
Anrighe <assoonpc@gmail.com>
2023-04-14 12:19:46 +0200

    Tutorial 3 LoopWalk

--- Tutorial-03/lib/LoopWalk.cpp
+++ Tutorial-03/lib/LoopWalk.cpp
@@ -14,27 +14,82 @@ class LoopWalkPass final : public LoopPass
 
 	LoopWalkPass() : LoopPass(ID) {}
 
-	virtual void getAnalysisUsage(AnalysisUsage &AU) const override 
-	{
-
-	}
+	virtual void getAnalysisUsage(AnalysisUsage &AU) const override {}
 
 	virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override 
 	{
-		outs() << "\nLOOPPASS INIZIATO...\n";
+		outs()<<"\nLOOPPASS INIZIATO...\n";
 
-		//Verifica che il loop sia in forma normalizzata
+		// ----------------- ESERCIZIO 1 ------------------
+		// Verifica che il loop sia in forma normalizzata
 		if ((*L).isLoopSimplifyForm())
 		{
-			outs()<<"L è un Loop in forma normalizzata\n";
+			outs()<<"Il loop è in forma normalizzata\n\n";
 			
 			// Controlla che il loop abbia un preheader
-			if((*L).getLoopPreheader() != NULL)
+			BasicBlock * headerBlock = (*L).getLoopPreheader();
+			if(headerBlock != NULL)
 			{
-				//Dobbiamo fare una variabile basic block dove salvare il ritorno di getlooppreheader
-				outs()<<"L ha un preheader\n";
+				outs()<<"Il loop ha un preheader: ";
+
+				// Stampa del preheader
+				outs()<<*headerBlock<<"\n";
+				
+				// Stampa dei Basic Block del loop
+				outs()<<"Basic Block del loop: ";
+				for (auto &BBIter : (*L).getBlocks())
+					outs()<<*BBIter<<"\n";
+				
+				// ----------------- ESERCIZIO 2 ------------------
+				// Itera su tutte le istruzioni del loop
+				for (auto &BBIter : (*L).getBlocks())
+				{
+					for (auto &InstIter : *BBIter)
+					{
+						// Identifica l'istruzione SUB
+						if (InstIter.getOpcode() == Instruction::Sub)
+						{
+							ConstantInt * firstOperand = dyn_cast<ConstantInt>(InstIter.getOperand(0));
+							ConstantInt * secondOperand = dyn_cast<ConstantInt>(InstIter.getOperand(1));
+
+							// Controlla gli operandi della SUB e per quelli non costanti stampa l'istruzione ad essi associati
+							if (!firstOperand)
+							{
+								Instruction * firstOperandInstruction = dyn_cast<Instruction>(InstIter.getOperand(0));
+								outs()<<"Istruzione del primo operando:"<<*firstOperandInstruction<<"\n";
+
+								// Stampa il Basic Block che contiene l'istruzione 
+								outs()<<"Basic Block che contiene l'istruzione:"<<*((*firstOperandInstruction).getParent());
+							}
+							else
+								outs()<<"Il primo operando è una costante di valore "<<(*firstOperand).getValue()<<"\n";
+							
+							if (!secondOperand)
+							{
+								Instruction * secondOperandInstruction = dyn_cast<Instruction>(InstIter.getOperand(1));
+								outs()<<"Istruzione del secondo operando:"<<*secondOperandInstruction<<"\n";
+
+								// Stampa il Basic Block che contiene l'istruzione 
+								outs()<<*((*secondOperandInstruction).getParent());
+
+							}
+							else
+								outs()<<"Il secondo operando è una costante di valore "<<(*secondOperand).getValue()<<"\n";
+								
+						}
+					}
+					
+				}
+				
+					
+
 			}
+			else
+				outs()<<"Il loop NON ha un preheader\n";
 		}
+		else
+			outs()<<"Il loop NON è in forma normalizzata\n\n";
+
 
 
 
