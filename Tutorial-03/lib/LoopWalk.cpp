2eedbbd4ce70c022faa6c865a2107dc83894ea14
gabriele.cusato <284647@studenti.unimore.it>
2023-04-15 10:39:47 +0200

    fatto tutorial-03

--- Tutorial-03/lib/LoopWalk.cpp
+++ Tutorial-03/lib/LoopWalk.cpp
@@ -15,7 +15,44 @@ public:
   }
 
   virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override {
-    outs() << "\nLOOPPASS INIZIATO...\n"; 
+    outs() << "\nLOOPPASS INIZIATO...\n";
+    outs() << "ESERCIZIO 1 e 2\n";
+    if((*L).isLoopSimplifyForm())
+    {
+      if(BasicBlock *preheader = (*L).getLoopPreheader())
+      {
+        outs()<<"PreHeader: "<<*preheader;
+        for (Loop::block_iterator BI = L->block_begin(); BI != L->block_end(); ++BI)
+        {
+          BasicBlock *B = *BI;
+          //outs()<<*B<<"\n";
+          for(auto Iter = (*B).begin(); Iter != (*B).end(); ++Iter )
+          {
+            if( (*Iter).getOpcode() == Instruction::Sub ) // controllo se l'operazione che viene effettuata è una MUL
+            {
+              Value *Operand1 = (*Iter).getOperand(0);
+              Value *Operand2 = (*Iter).getOperand(1);
+              ConstantInt *C = dyn_cast<ConstantInt>(Operand1);
+              ConstantInt *D = dyn_cast<ConstantInt>(Operand2);
+
+              if(C == nullptr)
+              {
+                Instruction *reference = dyn_cast<Instruction>(Operand1);
+                outs()<<*reference<<"\n";
+                outs()<<*(*reference).getParent()<<"\n";
+              }
+              if(D == nullptr)
+              {
+                Instruction *reference = dyn_cast<Instruction>(Operand2);
+                outs()<<*reference<<"\n";
+                outs()<<*(*reference).getParent()<<"\n";
+              }
+            }
+          }
+        }
+      }
+    }
+
     return false; 
   }
 };
