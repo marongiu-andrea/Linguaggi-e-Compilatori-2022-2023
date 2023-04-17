03b3b7e8d05b51cb03e1ffd8b1b2502f6cbb838e
Gabriel Panini <127861729+gabbogithub@users.noreply.github.com>
2023-04-17 16:14:25 +0200

    Soluzione esercizi loop

--- Tutorial-03/lib/LoopWalk.cpp
+++ Tutorial-03/lib/LoopWalk.cpp
@@ -1,6 +1,8 @@
 #include <llvm/Analysis/LoopPass.h>
 #include <llvm/Analysis/ValueTracking.h>
-
+#include </usr/include/llvm/Analysis/LoopPass.h>
+#include <llvm/Analysis/LoopInfo.h>
+#include <llvm/IR/Instruction.h>
 using namespace llvm;
 
 namespace {
@@ -15,8 +17,48 @@ public:
   }
 
   virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override {
-    outs() << "\nLOOPPASS INIZIATO...\n"; 
+    outs() << "\nLOOPPASS INIZIATO...\n";
+    if (L->isLoopSimplifyForm())
+      outs() << "Il Loop e' in forma normalizzata\n";
+    else
+      outs() << "Il loop non e' in forma normalizzata\n";
+
+    BasicBlock *preheader = L->getLoopPreheader();
+    if (preheader)
+      outs() << "Il loop ha un preheader: " << *preheader;
+    else
+      outs() << "Il loop non ha preheader\n";
+
+    outs() << "Questi sono i basic blocks che formano il loop:\n";
+    ArrayRef<BasicBlock*> loopBlocks = L->getBlocks();
+    for (auto block : loopBlocks)
+      outs() << *block;
+    
+    outs () << "Queste sono le sottrazioni con istruzioni come operandi:\n";
+    for (Loop::block_iterator BI = L->block_begin(); BI != L->block_end() ; ++BI){
+      for (auto instr = (*BI)->begin(); instr != (*BI)->end() ; ++instr)
+        if (instr->getOpcode() == Instruction::Sub){
+          outs() << "La sottrazione" << *instr << " ha le seguenti istruzioni come operandi: \n";
+          printInstruction(*instr);
+        }
+      }
     return false; 
+  }
+
+  void printInstruction(const Instruction &instr){
+    Instruction *op1 = dyn_cast<Instruction>(instr.getOperand(0));
+    Instruction *op2 = dyn_cast<Instruction>(instr.getOperand(1));
+    if(op1){
+      outs() << *op1 << " definita nel blocco: ";
+      op1->getParent()->printAsOperand(outs());
+      outs() << "\n";
+    }
+
+    if(op2){
+      outs() << *op2 << " definita nel blocco: ";
+      op2->getParent()->printAsOperand(outs());
+      outs() << "\n";
+    }
   }
 };
 
