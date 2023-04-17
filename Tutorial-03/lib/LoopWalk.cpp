58cf699ce4edcc4396b5b634bcaf0bd037e0836c
Davide Buchignani <davi28564@gmail.com>
2023-04-17 13:12:54 +0200

    Refactor LoopWalk

--- Tutorial-03/lib/LoopWalk.cpp
+++ Tutorial-03/lib/LoopWalk.cpp
@@ -25,42 +25,38 @@ public:
       BasicBlock *B1 = L->getLoopPreheader();
       outs() << "\nPreheader del loop: " <<  *B1 << "\n";
 
-      // stampo i basic blocks del loop
+      // itero sui basic blocks del loop
       int i = 1;
       for (Loop::block_iterator BI = L->block_begin(); BI != L->block_end(); ++BI){
-
         BasicBlock *BB = *BI;
-
         outs() << "\nBasic block n. " << i << ": " << *BB << "\n";
         i++;
-      }
 
-    }
-    
-    outs() << "Scrorrendo le istruzioni del Loop: \n";
-    for (Loop::block_iterator BI = L->block_begin(); BI != L->block_end(); ++BI)
-    {
-      BasicBlock *bb = *BI;
-      for(auto Iter = bb->begin(); Iter != bb->end(); ++Iter)
-      {
-        Instruction &Inst = *Iter;
-        if(Inst.getOpcode() == Instruction::Sub)
-        {
-          outs() << "Istruzione SUB: " << Inst << "\n";
-          for(auto *opIter = Inst.op_begin(); opIter != Inst.op_end(); ++opIter)
+        outs() << "Scrorrendo le istruzioni del BB: \n";
+        for(auto InstIter = BB->begin(); InstIter != BB->end(); ++InstIter){
+          Instruction &Inst = *InstIter;
+
+          //cerco la SUB
+          if(Inst.getOpcode() == Instruction::Sub)
           {
-            Value *op = *opIter;
-            if(Instruction *arg = dyn_cast<Instruction>(op))
-            {
-              outs() << "Istruzione che definisce: " << *op <<"\n";
-              outs() << "Basic Block dell'istruzione:\n" << *arg->getParent() << "\n";
+            outs() << "Istruzione SUB: " << Inst << "\n";
+
+            //Itero su gli operatori
+            for(auto *opIter = Inst.op_begin(); opIter != Inst.op_end(); ++opIter){
+              Value *op = *opIter;
+
+              //Se non e' una const
+              if(Instruction *arg = dyn_cast<Instruction>(op))
+              {
+                outs() << "Istruzione che definisce: " << *op <<"\n";
+                outs() << "Basic Block dell'istruzione:\n" << *arg->getParent() << "\n";
+              }
             }
           }
         }
       }
+      return true;
     }
-    
-
     return false; 
   }
 };
