41d9a9e923b1b7e11fc81a4cfcb0e279c9b1304d
s0SimoneP0s <53236@studenti.unimore.it>
2023-04-19 09:41:32 +0200

    Consegna Assignement 2 - Gruppo 10

--- Tutorial-03/lib/LoopWalk.cpp
+++ Tutorial-03/lib/LoopWalk.cpp
@@ -16,6 +16,40 @@ public:
 
   virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override {
     outs() << "\nLOOPPASS INIZIATO...\n"; 
+
+    outs() << "\n"; 
+
+    outs() << *L;  outs() << "\n"; 
+
+    // outs() <<  L->isLoopSimplifyForm(); outs() << "\n"; 
+    // outs() <<  *L->getLoopPreheader() ; outs() << "\n"; 
+    // outs() <<  *L->getHeader() ; outs() << "\n";
+
+    for (Loop::block_iterator BI = L->block_begin() ; BI != L->block_end();  ++BI) {
+      BasicBlock &p = (**BI);
+      //outs() << p ; outs() << "\n";
+      for (BasicBlock::iterator I = p.begin() ; I != p.end() ; ++I) {
+        //outs() << *I; outs() << "\n";
+        // usa un dynamic cast e un if 
+        // per trovare la sub %12  come esercizio loop walk pass pagina 12
+        switch (I->getOpcode())
+          case Instruction::Sub: {
+          outs() << "here a sum perhaps to be canceled\n";
+          outs() << *I; outs() << "\n";
+
+          // segui gli usi della SUB per cercare il basic block da cui derivano queste istruzioni
+
+
+          break;
+        }
+
+      }
+
+      
+    }
+
+
+
     return false; 
   }
 };
