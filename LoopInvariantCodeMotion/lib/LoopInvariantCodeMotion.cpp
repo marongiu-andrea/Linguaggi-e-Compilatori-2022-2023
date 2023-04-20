97c7d4b8aac4f3333a2e2c14c7d080c25badbd62
Enrico Marras <assoonpc@gmail.com>
2023-04-20 16:57:53 +0200

    new branch

---
+++ LoopInvariantCodeMotion/lib/LoopInvariantCodeMotion.cpp
@@ -0,0 +1,62 @@
+#include <llvm/Analysis/LoopPass.h>
+#include <llvm/Analysis/ValueTracking.h>
+#include <llvm/IR/Dominators.h>
+
+using namespace llvm;
+
+// opt -enable-new-pm=0 -load ./libLoopInvariantCodeMotion.so -loop-invariant-code-motion test/Loop.ll -disable-output
+
+//dobbiamo scrivere la nostra funzione per capire se uno statement è loop invarian
+//calcolare i dominatori -> si possono usare le istruzioni che llvm mette a disposizione
+//Trovare le uscite del loop
+//Trovare le istruzione candidate alla code motion
+
+//SI trovano in blocchi che dominano tutte le uscite del loop -> rilassabile se la 
+//		variabile definita dall'istruzione è dead all'uscita del loop
+
+/*
+1)Recuperare info
+
+Primo sweep del loop: -> se 
+2)Scrivere un pezzo di codice che ci dice se l'istruzione è loop invariant (NO API LLVM)
+3)Scrivere una funzione isSafeToMove()
+
+Secondo sweep in cui si spostano effettivamente le istruzioni:
+-> adesso tutte le istruzioni adatte sono marchiate per code motion
+4)Eseguite la code motion
+*/
+
+namespace {
+
+class LoopInvariantCodeMotionPass final : public LoopPass 
+{
+	public:
+	static char ID;
+
+	LoopInvariantCodeMotionPass() : LoopPass(ID) {}
+
+	virtual void getAnalysisUsage(AnalysisUsage &AU) const override 
+	{
+		// Imposta il Dominator Tree e le Loop Info come necessarie all'esecuzione del passo corrente
+		AU.addRequired<DominatorTreeWrapperPass>();
+		AU.addRequired<LoopInfoWrapperPass>();
+	}
+
+	virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override 
+	{
+		// Ottiene riferimenti al Dominator Tree e alle Loop Info
+		DominatorTree * DT = &getAnalysis<DominatorTreeWrapperPass>().getDomTree();
+		LoopInfo * LI = &getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+		outs()<<"Passo Loop Invariant Code Motion\n";
+		
+
+		return false; 
+	}
+};
+
+char LoopInvariantCodeMotionPass::ID = 0;
+RegisterPass<LoopInvariantCodeMotionPass> X("loop-invariant-code-motion",
+											"loop invariant code motion");
+
+} // anonymous namespace
+
