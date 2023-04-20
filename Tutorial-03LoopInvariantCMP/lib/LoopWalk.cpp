60a4cff7f298efe51c4ae237e9c28799ce543d50
Bondavalli <284643@studenti.unimore.it>
2023-04-20 17:13:53 +0200

    Inizio assigment invariant

---
+++ Tutorial-03LoopInvariantCMP/lib/LoopWalk.cpp
@@ -0,0 +1,94 @@
+#include <llvm/Analysis/LoopPass.h>
+#include <llvm/IR/Dominators.h>
+#include <llvm/Analysis/ValueTracking.h>
+
+using namespace llvm;
+
+namespace {
+
+class LoopWalkPass final : public LoopPass {
+public:
+  static char ID;
+
+  LoopWalkPass() : LoopPass(ID) {}
+
+  virtual void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<DominatorTreeWrapperPass>();
+    AU.addRequired<LoopInfoWrapperPass>();
+
+  //altri comandi:
+  //setPreservesAll()
+  //
+
+  // DominatorTree *DT = &getAnalysis<DominatorTreeWrapperPass>().getDomTree();
+  // da usare nel passo per richiedere il domTree, il codice prima non commentato setta il domtreewrapper come necessario
+  // al passo
+
+  // LoopInfo *LI = &getAnalysis<LoopInfoWrapperPass>().g;
+
+  }
+  Vector<Instruction> invariant;
+  Boolean isLoopInvariant(Instruction *i) {
+    
+    for(auto *opIter = Inst.op_begin(); opIter != Inst.op_end(); ++opIter){
+      Value *op = *opIter;
+      
+      //Se non e' una const
+      if(Instruction *arg = dyn_cast<Instruction>(op))
+      {
+        outs() << "Istruzione che definisce: " << *op <<"\n";
+        outs() << "Basic Block dell'istruzione:\n" << *arg->getParent() << "\n";
+      }
+    }
+  } 
+
+  virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override {
+    outs() << "\nLOOPPASS INIZIATO...\n"; 
+
+    DominatorTree *DT = &getAnalysis<DominatorTreeWrapperPass>().getDomTree();
+
+
+    // verificare la forma normalizzata
+    if (L->isLoopSimplifyForm()){
+      outs() << "\nLoop in forma normalizzata\n";
+      
+      // controllo preheader
+      BasicBlock *B1 = L->getLoopPreheader();
+      outs() << "\nPreheader del loop: " <<  *B1 << "\n";
+
+      // itero sui basic blocks del loop
+      int i = 1;
+      for (Loop::block_iterator BI = L->block_begin(); BI != L->block_end(); ++BI){
+        BasicBlock *BB = *BI;
+        outs() << "\nBasic block n. " << i << ": " << *BB << "\n";
+        i++;
+
+        outs() << "Scrorrendo le istruzioni del BB: \n";
+        for(auto InstIter = BB->begin(); InstIter != BB->end(); ++InstIter){
+          Instruction &Inst = *InstIter;
+
+            //Itero sugli operatori
+            for(auto *opIter = Inst.op_begin(); opIter != Inst.op_end(); ++opIter){
+              Value *op = *opIter;
+
+              //Se non e' una const
+              if(Instruction *arg = dyn_cast<Instruction>(op))
+              {
+                outs() << "Istruzione che definisce: " << *op <<"\n";
+                outs() << "Basic Block dell'istruzione:\n" << *arg->getParent() << "\n";
+              }
+            }
+        }
+      }
+      return true;
+    }
+    return false; 
+  }
+};
+
+char LoopWalkPass::ID = 0;
+RegisterPass<LoopWalkPass> X("loop-walk",
+                             "Loop Walk");
+
+} // anonymous namespace
+
