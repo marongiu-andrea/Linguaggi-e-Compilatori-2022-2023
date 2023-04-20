de5d93a8edd990a0abca20d3bbe5a41e3f11b11c
jot <singhamanjot726@gmail.com>
2023-04-20 16:36:27 +0200

    add loop instructions analysing
    Signed-off-by: jot <singhamanjot726@gmail.com>

--- Tutorial-03/lib/LoopWalk.cpp
+++ Tutorial-03/lib/LoopWalk.cpp
@@ -1,5 +1,7 @@
 #include <llvm/Analysis/LoopPass.h>
+#include <llvm/Analysis/LoopInfo.h>
 #include <llvm/Analysis/ValueTracking.h>
+#include <llvm/IR/Dominators.h>
 
 using namespace llvm;
 
@@ -12,10 +14,44 @@ public:
   LoopWalkPass() : LoopPass(ID) {}
 
   virtual void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<DominatorTreeWrapperPass>();
+    AU.addRequired<LoopInfoWrapperPass>();
   }
 
   virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override {
+    if (!L)
+      return false;
+
+    DominatorTree *dt = &getAnalysis<DominatorTreeWrapperPass>().getDomTree();
+    LoopInfo *li = &getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+    
     outs() << "\nLOOPPASS INIZIATO...\n"; 
+
+    outs() << "\nLoop normalizzato " << ((L->isLoopSimplifyForm()) ? "true" : "false") << "\n";
+
+    if (auto *preheader = L->getLoopPreheader()) {
+      outs() << "\npreheader " << *preheader;
+    }
+
+    for (auto *block : L->getBlocks()) {
+      for (auto &instr: *block) {
+        if (instr.getOpcode() == Instruction::Sub) {
+          outs() << "\ninstruction " << instr;
+
+          outs() << "\n operands: ";
+          for (auto op = instr.op_begin(); op != instr.op_end(); ++op) {
+            outs() << "\n ";
+            if (auto *instr = dyn_cast<Instruction>(*op)) {
+              outs() << "\n " << *instr;
+            }
+          }
+          outs() << "\nbasic block " << *block;
+          outs() << "\n\n\n";
+        }
+      }
+    }
+
+    outs() << "\n";
     return false; 
   }
 };
