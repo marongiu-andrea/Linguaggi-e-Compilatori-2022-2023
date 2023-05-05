d1aaaf0ba7b81ff17b5a3193b08a6ae6e39264ab
Riccardo Cracco <281925@studenti.unimore.it>
2023-05-05 14:47:55 +0200

    Implementato primo punto di loop fusion.

--- LoopFusion/lib/LoopFusion.cpp
+++ LoopFusion/lib/LoopFusion.cpp
@@ -1,16 +1,61 @@
 #include "LoopFusion.h"
 
+#include <llvm/Analysis/LoopAnalysisManager.h>
+#include <llvm/Analysis/LoopInfo.h>
 #include <llvm/IR/BasicBlock.h>
 #include <llvm/IR/Function.h>
 #include <llvm/IR/PassManager.h>
+#include <llvm/Pass.h>
 #include <llvm/Passes/PassBuilder.h>
 #include <llvm/Passes/PassPlugin.h>
+#include <llvm/Transforms/Utils/LoopSimplify.h>
 
 using namespace llvm;
 
 PreservedAnalyses LoopFusion::run(Function& function, FunctionAnalysisManager& fam)
 {
-    outs() << "Hello world!\n";
+    LoopInfo& loopInfo = fam.getResult<LoopAnalysis>(function);
+    SmallVector<Loop*, 4> loops = loopInfo.getLoopsInPreorder();
+
+    if (loops.size() == 0)
+    {
+        outs() << "Non ci sono loop nella funzione.\n";
+    }
+    else if (loops.size() == 1)
+    {
+        outs() << "Nella funzione c'è un solo loop. Impossibile dare informazioni sull'adiacenza.\n";
+    }
+    else
+    {
+        for (unsigned int i = 0; i < loops.size() - 1; i++)
+        {
+            unsigned int j = i + 1;
+            Loop* loopi = loops[i];
+            Loop* loopj = loops[i + 1];
+            BasicBlock* loopiExitBlock = loopi->getExitBlock();
+            BasicBlock* loopjPreheader = loopj->getLoopPreheader();
+
+            if (loopiExitBlock == nullptr)
+            {
+                outs() << "loop[i] ha più di un'uscita. Impossibile dare informazioni sull'adiacenza.\n";
+            }
+            else if (loopjPreheader == nullptr)
+            {
+                outs() << "loop[j] non è in forma normalizzata. Impossibile dare informazioni sull'adiacenza.\n";
+            }
+            else
+            {
+                if (loopiExitBlock == loopjPreheader)
+                {
+                    outs() << "loop[" << i << "] è adiacente a loop[" << j << "].\n";
+                }
+                else
+                {
+                    outs() << "loop[" << i << "] non è adiacente a loop[" << j << "].\n";
+                }
+            }
+        }
+    }
 
     return PreservedAnalyses::all();
 }
