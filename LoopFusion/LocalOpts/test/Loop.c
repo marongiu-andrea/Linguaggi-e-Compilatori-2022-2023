7b3c6e9041ee96f688fa43f300eb5d777088fb94
Alex <ferrarini.alex24@gmail.com>
2023-05-11 17:09:31 +0200

    added .svg graphs

--- LoopFusion/LocalOpts/test/Loop.c
+++ LoopFusion/LocalOpts/test/Loop.c
@@ -1,8 +1,10 @@
+#include <stdio.h>
 #define N 100
 void populate(int a[N], int b[N], int c[N]){
     int i;
     for(i=0;i<N;i++)
         a[i] = 5*c[i];
+    printf("%d",a[i]);
     for(i=0;i<N;i++)
         b[i] = a[i]+c[i];
 }
