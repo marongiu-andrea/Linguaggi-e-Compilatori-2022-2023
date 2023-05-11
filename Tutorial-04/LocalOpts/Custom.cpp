fcffbc02c12e23e226a2b68b30fd0539d163d1e1
carlouguzzoni1 <carlouguzzoni@gmail.com>
2023-05-11 17:47:32 +0200

    check 1 algoritmo + inizio loop fusion

--- Tutorial-04/LocalOpts/Custom.cpp
+++ Tutorial-04/LocalOpts/Custom.cpp
@@ -1,16 +1,18 @@
 #include "LocalOpts.h"
 #include "llvm/IR/InstrTypes.h"
 
-using namespace llvm;
-
 #include <iostream>
 #include <set>
 #include <vector>
 #include <algorithm>
 #include <map>
 
+using namespace llvm;
+
 #include "llvm/ADT/SmallVector.h"
 
+
+// Funzione appoggio per ordinare gli insiemi di loop CFG-equivalenti secondo dominanza.
 struct less_than_key
 {
   DominatorTree &DT;
@@ -25,28 +27,33 @@ struct less_than_key
   }
 };
 
+
 llvm::PreservedAnalyses TransformPass::run([[maybe_unused]] llvm::Function &F, llvm::FunctionAnalysisManager &AM) {
   
+  // Analisi utili nel seguito.
   auto &LI = AM.getResult<LoopAnalysis>(F);
   auto &DT = AM.getResult<DominatorTreeAnalysis>(F);
   auto &PDT = AM.getResult<PostDominatorTreeAnalysis>(F);
-  llvm::SmallVector<Loop*> LoopVector = LI.getLoopsInPreorder();
-  std::map<Loop*, bool> LoopsAssigned;
+
+
+  llvm::SmallVector<Loop*> LoopVector = LI.getLoopsInPreorder();  // Vector dei loop.
+  std::map<Loop*, bool> LoopsAssigned;                            // Lookup per loop assegnati ad insiemi.
 
   for (auto iter_loop = LoopVector.begin(); iter_loop != LoopVector.end(); ++iter_loop) {
     Loop *L = *iter_loop;
 
-    outs() << "Loop in forma std:\n";
     if (L->isLoopSimplifyForm()) {
+      outs() << "Loop in forma std:\n";
       outs() << *L << "\n";
+
       LoopsAssigned[L] = false;
     }
-    // Ricordarsi di eliminare loop non in nofmal form.
+    // Ricordarsi di eliminare loop non in normal form.
   }
 
   std::cout << "LoopVector size: " << LoopVector.size() << std::endl;
   
-  std::vector<std::vector<Loop*>> CFGEquivalents;
+  std::vector<std::vector<Loop*>> CFGEquivalents;   // Vector degli insiemi di loop CFG-equivalent.
 
   for (auto iter_i = LoopVector.begin(); iter_i != LoopVector.end(); ++iter_i) {
     Loop *L1 = *iter_i;
@@ -108,14 +115,53 @@ llvm::PreservedAnalyses TransformPass::run([[maybe_unused]] llvm::Function &F, l
 
       for (auto k = ++j; k != V.end(); ++k) {
         Loop* L2 = *k;
-
-        if (L1->getExitBlock() != L2->getLoopPreheader())
-          continue;
-
         
+        // Controllo n°1.
+        if (L1->getExitBlock() == L2->getLoopPreheader()) {
+          int instruction_count = 0;
+          BasicBlock *MiddleBlock = L1->getExitBlock();
+
+          for (auto iter_block = MiddleBlock->begin(); iter_block != MiddleBlock->end(); ++iter_block) {
+            ++instruction_count;
+          }
+
+          if (instruction_count != 1) {
+            // std::cout << "Loop non adiacenti!" << std::endl;
+            continue;
+          }
+
+          /*
+          Instruction &I = *MiddleBlock->begin();
+          BranchInst *BI = dyn_cast<llvm::BranchInst*>(I);
+
+          if (!(BI && BI->getSuccessor(0) == L2->getHeader())) {
+            continue;
+          }
+          */
+        }
+
+        BasicBlock *L1L = L1->getLoopLatch();
+        Instruction *I1 = dyn_cast<Instruction>(L1L->end());
+        BranchInst *BI1 = dyn_cast<llvm::BranchInst>(I1);
+        BI1->setSuccessor(0, L2->getHeader());
+        
+        BasicBlock *HB1 = L1->getHeader();
+        Instruction *I2 = dyn_cast<Instruction>(HB1->end());
+        BranchInst *BI2 = dyn_cast<llvm::BranchInst>(I2);
+        BI2->setSuccessor(1, L2->getHeader());
+
+        BasicBlock *H2L = L2->getLoopLatch();
+        Instruction *I3 = dyn_cast<Instruction>(H2L->end());
+        BranchInst *BI3 = dyn_cast<llvm::BranchInst>(I3);
+        BI2->setSuccessor(0, L1->getHeader());
+
+        std::cout << "Loop 1 latch:" << std::endl;
+        outs() << *L1L << "\n";
       }
     }
   }
+
+
 
   PreservedAnalyses PA = PreservedAnalyses();
   return PA;
