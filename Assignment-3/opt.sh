41de32cb78f2b1dcf9133a6e77373d6d9313acb8
simone <simonevaiasinni@gmail.com>
2023-05-11 15:11:28 +0200

    aggiunto il README

--- Assignment-3/opt.sh
+++ Assignment-3/opt.sh
@@ -1,14 +1,7 @@
 #! /bin/bash
 
-pass="None" 
-
-if [[ ! -z $pass ]]
-then
-  srcfile="test/Loop.ll"          
-  opt -enable-new-pm=0 -load ./libLoopWalk.so -loop-walk $srcfile -disable-output
-else
-  echo "[Error] Invalid pass"
-fi
-
-
-
+pass=loop-invariant-code-motion
+optimizedbc=Loop.optimized.bc
+optimizedll=Loop.optimized.ll
+opt -enable-new-pm=0 -load ./licm.so -$pass test/Loop.ll -o $optimizedbc
+llvm-dis $optimizedbc -o $optimizedll
