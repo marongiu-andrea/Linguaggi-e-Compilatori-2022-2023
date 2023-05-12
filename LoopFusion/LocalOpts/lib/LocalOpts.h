b4344ccce4da2397bcab034cf8f68e34d254d3f8
LazzariniMattia <127770919+LazzariniMattia@users.noreply.github.com>
2023-05-12 13:04:51 +0200

    Add files via upload

--- LoopFusion/LocalOpts/lib/LocalOpts.h
+++ LoopFusion/LocalOpts/lib/LocalOpts.h
@@ -2,7 +2,9 @@
 
 #include <llvm/IR/PassManager.h>
 #include <llvm/IR/Constants.h>
-
+#include <llvm/IR/Dominators.h>
+#include "llvm/Analysis/ScalarEvolution.h"
+#include "llvm/Analysis/PostDominators.h"
 
 class LoopFusionPass final : public llvm::PassInfoMixin<LoopFusionPass> 
 {
@@ -10,5 +12,6 @@ class LoopFusionPass final : public llvm::PassInfoMixin<LoopFusionPass>
   		llvm::PreservedAnalyses run([[maybe_unused]] llvm::Function &, llvm::FunctionAnalysisManager &);
 	private:
 		bool areLoopsAdjacent(llvm::BasicBlock * exitBlockL1, llvm::BasicBlock * preheaderL2, llvm::BasicBlock * headerL2);
+		bool checkTripCount(llvm::Loop * IterLoop1, llvm::Loop * IterLoop2, llvm::ScalarEvolution * SE);
+		bool areLoopsControlFlowEquivalent(llvm::Loop * IterLoop1, llvm::Loop * IterLoop2, llvm::DominatorTree * DT, llvm::PostDominatorTree *PDT);
 };
-
--- LoopFusion/LocalOpts/lib/LoopFusion.cpp
+++ LoopFusion/LocalOpts/lib/LoopFusion.cpp
@@ -11,14 +11,14 @@ using namespace llvm;
 // -----------------------------------------------------------------------------------
 // Per generare il file .ll da Loop.c
 //  clang -O0 -Xclang -disable-O0-optnone -emit-llvm -c Loop.c
-//  opt -passes=mem2reg Loop.bc -o Loop.opt.bc
+//  opt-14 -passes=mem2reg Loop.bc -o Loop.opt.bc
 //  llvm-dis Loop.opt.bc -o Loop.opt.ll
 
 // mem2reg - The mem2reg pass converts non-SSA form of LLVM IR into SSA form,
 //  raising loads and stores to stack-allocated values to “registers” (SSA values).
 // -----------------------------------------------------------------------------------
 
-// opt -load-pass-plugin=./libLocalOpts.so -passes=loopfusion test/Loop.opt.ll -o test/test.loopfusion.optimized.bc
+// opt-14 -load-pass-plugin=./libLocalOpts.so -passes=loopfusion test/Loop.opt.ll -o test/test.loopfusion.optimized.bc
 // llvm-dis test/test.loopfusion.optimized.bc -o test/test.loopfusion.optimized.ll
 // -----------------------------------------------------------------------------------
 
@@ -39,6 +39,27 @@ bool LoopFusionPass::areLoopsAdjacent(BasicBlock * exitBlockL1, BasicBlock * pre
 		}
 	}
 	outs()<<"I due loop NON sono adiacenti\n";
+	return false;
+}
+
+bool LoopFusionPass::LoopFusionPass::checkTripCount(Loop * IterLoop1, Loop * IterLoop2, ScalarEvolution * SE) {
+	const SCEV* tripCountL1 = SE->getBackedgeTakenCount(IterLoop1);
+	const SCEV* tripCountL2 = SE->getBackedgeTakenCount(IterLoop2);
+					
+	// Controlla che il trip count tra i due loop sia calcolabile e che sia uguale
+	if ((!isa<SCEVCouldNotCompute>(tripCountL1)) && (!isa<SCEVCouldNotCompute>(tripCountL2)) && (tripCountL1 == tripCountL2))
+		return true;
+	
+	return false;
+}
+
+bool LoopFusionPass::areLoopsControlFlowEquivalent(Loop * IterLoop1, Loop * IterLoop2, DominatorTree * DT, PostDominatorTree * PDT) {
+	// Se IterLoop1 domina IterLoop2 e se IterLoop2 post-domina IterLoop1 sono Control-Flow Equivalent
+	if ((*DT).dominates((*IterLoop1).getLoopPreheader(), (*IterLoop2).getLoopPreheader()) && (*PDT).dominates((*IterLoop2).getLoopPreheader(), (*IterLoop1).getLoopPreheader())) {
+		outs()<<"Ho trovato due loop Control-Flow Equivalent:\n";
+		return true;
+	}
+
 	return false;
 }
 
@@ -66,17 +87,13 @@ llvm::PreservedAnalyses LoopFusionPass::run([[maybe_unused]] Function &F, Functi
 				{
 					outs()<<"I Loop:\n"<<*IterLoop1<<*IterLoop2<<" sono adiacenti\n----------------------\n";
 					
-					const SCEV* tripCountL1 = SE.getBackedgeTakenCount(IterLoop1);
-					const SCEV* tripCountL2 = SE.getBackedgeTakenCount(IterLoop2);
-					
-					// Controlla che il trip count tra i due loop sia calcolabile e che sia uguale
-					if ((!isa<SCEVCouldNotCompute>(tripCountL1)) && (!isa<SCEVCouldNotCompute>(tripCountL2)) && (tripCountL1 == tripCountL2))
-					{
+					// Effettuo le analisi sul Trip Count
+					if (checkTripCount(IterLoop1, IterLoop2, &SE)) {
 						outs()<<"I Loop:\n"<<*IterLoop1<<*IterLoop2<<" hanno lo stesso trip count\n----------------------\n";
 
 						// TODO : CONTROLLARE CHE I BOUND DEI DUE LOOP SIANO UGUALI (MAGARI ANCHE LE VARIABILI D'INDUZIONE?)
 
-						// Due Loop sono Control-Flow Equivalent se l'esecuzione di uno assicura l'esecuzione dell'altro.
+						// Due Loop sono Con trol-Flow Equivalent se l'esecuzione di uno assicura l'esecuzione dell'altro.
 						//  Per verificare che due loop siano Control-Flow Equivalent si usa l'analisi dei dominatori e
 						//   dei post-dominatori:
 						//    Se il Loop j domina il Loop k, e il Loop k post-domina il loop j
@@ -85,53 +102,17 @@ llvm::PreservedAnalyses LoopFusionPass::run([[maybe_unused]] Function &F, Functi
 						// Dominator: un nodo x domina un nodo y se ogni percorso dall'entry block a y contiene x
 						// Post-dominator: un nodo y post-domina un nodo x se ogni percorso da x all'uscita contiene y
 
-						// Se IterLoop1 domina IterLoop2 e se IterLoop2 post-domina IterLoop1 sono Control-Flow Equivalent
-						if ((*DT).dominates((*IterLoop1).getLoopPreheader(), (*IterLoop2).getLoopPreheader()) 
-							&& (*PDT).dominates((*IterLoop2).getLoopPreheader(), (*IterLoop1).getLoopPreheader()))
-						{
-							outs()<<"Ho trovato due loop Control-Flow Equivalent:\n";
-							outs()<<"Il Loop: "<<*IterLoop1<<" domina ---> "<<*IterLoop2<<"\n";
-							outs()<<"Il Loop: "<<*IterLoop2<<" post-domina ---> "<<*IterLoop1<<"\n";
-							
-							/*
-							// PROBABILMENTE DA NON FARE (?)
-							
-							//4) There cannot be any negative distance dependencies between the loops. 
-							//    If all of these conditions are satisfied, it is safe to fuse the loops.
-							// 	   There cannot be any negative distance dependencies between Lj and Lk
-							//	    A negative distance dependence occurs between Lj and Lk, Lj before Lk, 
-							//   	 when at iteration m Lk uses a value
-							//		  that is computed by Lj at a future iteration m+n (where n > 0)
-
-							// Use SCEV to determine if there could be negative dependencies between the two loops
-
-							bool HasNegativeDependencies = false;
-
-							if (!SE->isLoopInvariant(tripCountL1) && !SE->isLoopInvariant(tripCountL2)) 
-							{
-								if (SE->hasComputableLoopEvolution(tripCountL1) && SE->hasComputableLoopEvolution(tripCountL2)) 
-								{
-									// !!!!! Calcolo del delta per verificare se ci siano dipendenze negative tra le induction variable dei due loop 
-									const SCEV *Delta = SE->getMinusSCEV(inductionVarL1, inductionVarL2);
-									if (!SE->isLoopInvariant(Delta)) 
-									{
-										HasNegativeDependencies = true;
-									}
-								}
-							}
-
-							// Se i due loop non hanno dipendenze negative
-							if (!HasNegativeDependencies) 
-							{
-								
-							} 
-							*/
-						}		
+						// Verifico che i Loops siano Control Flow Equivalent
+						if(areLoopsControlFlowEquivalent(IterLoop1, IterLoop2, DT, PDT)) {
+							outs()<<"HO FINITO\n";
+						}
 					}
 				}			
 			}
 		}
 	}
+	
+	loops.clear();
   	return PreservedAnalyses::none();
 }
 
