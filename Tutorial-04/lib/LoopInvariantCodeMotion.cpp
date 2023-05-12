fd0899970f57b9966a3d9aa107f4e56726579adb
AngeloLonganoUni <285288@studenti.unimore.it>
2023-05-12 15:47:48 +0200

    [FEAUTURE] Finished code-motion and insert realtion pdf on Tutorial-04

--- Tutorial-04/lib/LoopInvariantCodeMotion.cpp
+++ Tutorial-04/lib/LoopInvariantCodeMotion.cpp
@@ -1,7 +1,6 @@
 #include <llvm/Analysis/LoopPass.h>
 #include <llvm/IR/BasicBlock.h>
 #include <llvm/Analysis/ValueTracking.h>
-#include "llvm/Analysis/LoopInfo.h"
 #include "llvm/IR/Dominators.h"
 
 using namespace llvm;
@@ -12,7 +11,100 @@ namespace
 
   bool isLoopInvariant(BinaryOperator *I, Loop *L);
   bool isLoopInvariantOperand(Value *op, Loop *L);
+  bool checkDominatesAllExits(Loop *L, BinaryOperator *I, DominatorTree &DT);
 
+  bool usesVariable(Instruction *I, Value *V);
+  bool checkDominatesAllUsers(Loop *L, BinaryOperator *assignment, DominatorTree &DT);
+
+  class LoopInvariantCodeMotion final : public LoopPass
+  {
+  public:
+    static char ID;
+
+    LoopInvariantCodeMotion() : LoopPass(ID) {}
+
+    virtual void getAnalysisUsage(AnalysisUsage &AU) const override
+    {
+      AU.addRequired<DominatorTreeWrapperPass>();
+      AU.setPreservesAll();
+    }
+
+    virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override
+    {
+      DominatorTree &DT = getAnalysis<DominatorTreeWrapperPass>().getDomTree();
+
+      outs()
+          << "\nLOOPPASS INIZIATO...\n";
+      if (L->isLoopSimplifyForm())
+      {
+        outs() << "FORMA NORMALIZZATA\n";
+      }
+      BasicBlock *BB = L->getLoopPreheader();
+      if (BB)
+      {
+        outs() << "PREHEADER " << *BB << "\n";
+      }
+      for (Loop::block_iterator BI = L->block_begin(); BI != L->block_end(); ++BI)
+      {
+        BasicBlock *B = *BI;
+
+        outs() << "Basic Block: ";
+        B->printAsOperand(outs(), false);
+        outs() << "\n";
+
+        for (auto Iter = B->begin(); Iter != B->end(); ++Iter)
+        {
+          outs() << *Iter;
+
+          BinaryOperator *binOpInstruction = dyn_cast<BinaryOperator>(Iter);
+          if (binOpInstruction)
+          {
+            outs() << "\t###Vediamo dentro###\n";
+            // TODO: capire se una istruzione è loop invariant
+            if (isLoopInvariant(binOpInstruction, L))
+            {
+              if (checkDominatesAllExits(L, binOpInstruction, DT))
+              {
+                if (checkDominatesAllUsers(L, binOpInstruction, DT))
+                {
+                  outs() << "\t**ISTRUZIONE LOOP INVARIANT E DOMINANTE SU TUTTE LE USCITE E TUTTI I BLOCCHI CHE USANO LA VARIABILE**\n";
+                  instructionsToMove.push_back(binOpInstruction);
+                }
+                else
+                {
+                  outs() << "\t**ISTRUZIONE LOOP INVARIANT E DOMINANTE SU TUTTE LE USCITE MA NON SU TUTTI I BLOCCHI CHE USANO LA VARIABILE**\n";
+                }
+              }
+              else
+                outs() << "\t**ISTRUZIONE LOOP INVARIANT NON DOMINA TUTTE LE USCITE DEL LOOP**\n";
+            }
+
+            else
+              outs() << "\t**ISTRUZIONE LOOP VARIANT**\n";
+          }
+          outs() << "\n";
+        }
+
+        outs() << "\n\n";
+      }
+      outs() << "\n\n"
+             << "ISTRUZIONE CHE RISPETTANO I REQUISITI DI CODE MOTION DAL LOOP\n";
+
+      for (auto *I : instructionsToMove)
+      {
+        outs() << *I << "\n";
+        I->moveBefore(L->getLoopPreheader()->getTerminator());
+      }
+
+      return false;
+    }
+  };
+
+  char LoopInvariantCodeMotion::ID = 0;
+  RegisterPass<LoopInvariantCodeMotion> X("loop-invariant-code-motion",
+                                          "Loop Invariant Code Motion");
+
+  // FUNZIONI DI SUPPORTO
   bool isLoopInvariantOperand(Value *op, Loop *L)
   {
     outs() << "\tReaching definitions: " << *op << "\n";
@@ -69,7 +161,7 @@ namespace
     return (isLoopInvariantOperand(I->getOperand(0), L) && isLoopInvariantOperand(I->getOperand(1), L));
   }
 
-  bool checkDominator(Loop *L, BinaryOperator *I, DominatorTree &DT)
+  bool checkDominatesAllExits(Loop *L, BinaryOperator *I, DominatorTree &DT)
   {
     BasicBlock *loopExitBlock = L->getExitBlock();
     for (auto *exitPred : predecessors(loopExitBlock))
@@ -82,87 +174,39 @@ namespace
     }
     return true;
   }
-
-  class LoopInvariantCodeMotion final : public LoopPass
+  bool usesVariable(Instruction *I, Value *V)
   {
-  public:
-    static char ID;
-
-    LoopInvariantCodeMotion() : LoopPass(ID) {}
-
-    virtual void getAnalysisUsage(AnalysisUsage &AU) const override
+    for (Use &U : I->operands())
     {
-      AU.addRequired<LoopInfoWrapperPass>();
-      AU.addRequired<DominatorTreeWrapperPass>();
-      AU.setPreservesAll();
-      outs() << "getAnalysisUsage\n";
+      if (U.get() == V)
+      {
+        return true;
+      }
     }
+    return false;
+  }
 
-    virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override
+  bool checkDominatesAllUsers(Loop *L, BinaryOperator *assignment, DominatorTree &DT)
+  {
+    BasicBlock *loopHeader = L->getHeader();
+
+    // Verifica se la variabile di destinazione dell'assegnazione viene utilizzata in ogni blocco nel loop
+    for (auto *BB : L->blocks())
     {
-      DominatorTree &DT = getAnalysis<DominatorTreeWrapperPass>().getDomTree();
-
-      outs()
-          << "\nLOOPPASS INIZIATO...\n";
-      if (L->isLoopSimplifyForm())
+      for (auto &I : *BB)
       {
-        outs() << "FORMA NORMALIZZATA\n";
-      }
-      BasicBlock *BB = L->getLoopPreheader();
-      if (BB)
-      {
-        outs() << "PREHEADER " << *BB << "\n";
-      }
-      for (Loop::block_iterator BI = L->block_begin(); BI != L->block_end(); ++BI)
-      {
-        BasicBlock *B = *BI;
-
-        outs() << "Basic Block: ";
-        B->printAsOperand(outs(), false);
-        outs() << "\n";
-
-        for (auto Iter = B->begin(); Iter != B->end(); ++Iter)
+        if (usesVariable(&I, assignment))
         {
-          outs() << *Iter;
-
-          BinaryOperator *binOpInstruction = dyn_cast<BinaryOperator>(Iter);
-          if (binOpInstruction)
+          // Verifica se il blocco è dominato dall'istruzione di assegnazione
+          if (!DT.dominates(assignment, BB))
           {
-            outs() << "\t###Vediamo dentro###\n";
-            // TODO: capire se una istruzione è loop invariant
-            if (isLoopInvariant(binOpInstruction, L))
-            {
-              if (checkDominator(L, binOpInstruction, DT))
-              {
-                outs() << "\t**ISTRUZIONE LOOP INVARIANT E DOMINANTE**\n";
-                instructionsToMove.push_back(binOpInstruction);
-              }
-              else
-                outs() << "\t**ISTRUZIONE LOOP INVARIANT NON DOMINANTE**\n";
-            }
-
-            else
-              outs() << "\t**ISTRUZIONE LOOP VARIANT**\n";
+            // L'istruzione di assegnazione non domina il blocco che utilizza la variabile
+            return false;
           }
-          outs() << "\n";
         }
-
-        outs() << "\n\n";
       }
-      outs() << "\n\n"
-             << "INIZIO MOVIMENTO ISTRUZIONI\n";
-
-      for (auto *I : instructionsToMove)
-      {
-        outs() << *I << "\n";
-        I->moveBefore(L->getLoopPreheader()->getTerminator());
-      }
-
-      return false;
     }
-  };
 
-  char LoopInvariantCodeMotion::ID = 0;
-  RegisterPass<LoopInvariantCodeMotion> X("loop-invariant-code-motion",
-                                          "Loop Invariant Code Motion");
+    return true;
+  }
 } // namespace
