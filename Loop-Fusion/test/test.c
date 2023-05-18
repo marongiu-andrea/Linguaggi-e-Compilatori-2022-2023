0877c61741cdb80789f23c1b130cbe0130d37248
Shibodd <simonebondi456@gmail.com>
2023-05-18 17:04:04 +0200

    Added benchmarks.

--- Loop-Fusion/test/test.c
+++ Loop-Fusion/test/test.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-#define N 50
-
-void test(int* a, int* b, int* c) {
-    for (int i = 0; i < N; ++i)
+void test(int* a, int* b, int* c, int n) {
+    for (int i = 0; i < n; ++i)
         a[i] = 5 * c[i];
 
-    for (int i = 0; i < N; ++i)
+    for (int i = 0; i < n; ++i)
         b[i] = a[i] + c[i];
 }
