3d5af13787bdd957ad6bfe8486c206b3a3e4c2ff
Davide Buchignani <davi28564@gmail.com>
2023-06-01 11:23:12 +0200

    Refactor

--- Tutorial-LoopFusion/LocalOpts/lib/LoopFusion.cpp
+++ Tutorial-LoopFusion/LocalOpts/lib/LoopFusion.cpp
@@ -46,26 +46,26 @@ void mergeLoops(Loop* L1, Loop* L2, LoopInfo& LI) {
     }
   }
 
-  BasicBlock *L2_Exit = L2->getExitingBlock();
-  BasicBlock *L2_BodyStart = L2->getHeader()->getTerminator()->getSuccessor(0);
-  BasicBlock *L2_Latch = L2->getLoopLatch();
   BasicBlock *L1_Latch = L1->getLoopLatch();
   BasicBlock *L1_Header = L1->getHeader();
-  BasicBlock *L2_Header = L2->getHeader();
-
-  //qua si potrebbe usare anche direttamente getsuccessor dell'header per ottenere il body o va bene il for?
-
-  BasicBlock *L2_BodyEnd;
-  for(auto *BB : L2->getBlocks()){
-    if(BB == L2->getLoopLatch())
-      break;
-    L2_BodyEnd = BB;
-  }
   BasicBlock *L1_BodyEnd;
   for(auto *BB : L1->getBlocks()){
     if(BB == L1->getLoopLatch())
       break;
     L1_BodyEnd = BB;
+  }
+
+  BasicBlock *L2_Header = L2->getHeader();
+  BasicBlock *L2_BodyStart = L2->getHeader()->getTerminator()->getSuccessor(0);
+  BasicBlock *L2_Latch = L2->getLoopLatch();
+  BasicBlock *L2_Exit = L2->getExitingBlock();
+
+  //qua si potrebbe usare anche direttamente getsuccessor dell'header per ottenere il body o va bene il for?
+  BasicBlock *L2_BodyEnd;
+  for(auto *BB : L2->getBlocks()){
+    if(BB == L2->getLoopLatch())
+      break;
+    L2_BodyEnd = BB;
   }
 
   L1_Header->getTerminator()->setSuccessor(1, L2_Exit);
