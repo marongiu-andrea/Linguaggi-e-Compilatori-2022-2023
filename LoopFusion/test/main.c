88e2cede47927227a205ba5e664a4c47484af92c
Riccardo Cracco <281925@studenti.unimore.it>
2023-06-03 14:30:54 +0200

    Creata funzione di test.
    Corretto il nome della funzione nel main.

--- LoopFusion/test/main.c
+++ LoopFusion/test/main.c
@@ -1,21 +1,25 @@
+#include "test.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "test.h"
-extern void fun(int *a, int *b, int *c);
+
+extern void populate(int* a, int* b, int* c);
 #define N 1000
-int main()
+
+int main(int argc, char** argv)
 {
-	int *a = (int *)calloc(sizeof(int), SIZE);
-	int *b = (int *)calloc(sizeof(int), SIZE);
-	int *c = (int *)calloc(sizeof(int), SIZE);
-	clock_t start, end;
-	double cpu_time_used;
-	start = clock();
-	for(int i = 0; i < N; i++)
-		fun(a, b, c);
-	end = clock();
-	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-	printf("for loop took %f seconds to execute \n", cpu_time_used);
-	return 0;
+    int* a = (int*)calloc(sizeof(int), SIZE);
+    int* b = (int*)calloc(sizeof(int), SIZE);
+    int* c = (int*)calloc(sizeof(int), SIZE);
+
+    clock_t start, end;
+    double cpu_time_used;
+    start = clock();
+    for (int i = 0; i < N; i++)
+        populate(a, b, c);
+    end = clock();
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("for loop took %f seconds to execute \n", cpu_time_used);
+
+    return 0;
 }
--- LoopFusion/test/test.c
+++ LoopFusion/test/test.c
@@ -1,42 +1,15 @@
-#include <stdio.h>
+#define SIZE 100000
 
-#define SIZE 10
-
-void fun(int a[SIZE])
+void populate(int a[SIZE], int b[SIZE], int c[SIZE])
 {
     for (int i = 0; i < SIZE; i++)
     {
-        a[i] *= a[i];
-    }
-
-    printf("1° elemento dell'array: %d\n", a[0]);
-
-    for (int i = 0; i < SIZE; i++)
-    {
-        printf("%d\n", a[i]);
+        a[i] = i;
+        b[i] = a[i] * 2;
     }
 
     for (int i = 0; i < SIZE; i++)
     {
-        for (int j = 1; j < SIZE; j++)
-        {
-            if (a[i] > a[j])
-            {
-                int tmp = a[i];
-
-                a[i] = a[j];
-                a[j] = tmp;
-            }
-        }
-
-        for (int i = 0; i < SIZE; i++)
-        {
-            printf("%d\n", a[i]);
-        }
-    }
-
-    for (int i = 0; i < SIZE; i++)
-    {
-        printf("%d\n", a[i]);
+        c[i] = b[i];
     }
 }
